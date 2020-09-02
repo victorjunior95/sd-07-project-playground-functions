a466d8f5e975904df9925ccf0f8090839cf94a47
Isaac Batista <isaacbatst@gmail.com>
2020-09-02 13:25:35 -0300

    exercises 8 and 9

--- src/challenges.js
+++ src/challenges.js
@@ -33,17 +33,95 @@ function catAndMouse() {
   // seu código aqui
 }
 
+function wordReplacer(number) {
+  if (number % 3 === 0 && number % 5 === 0) {
+    return 'fizzBuzz';
+  }
+
+  if (number % 3 === 0) {
+    return 'fizz';
+  }
+
+  if (number % 5 === 0) {
+    return 'buzz';
+  }
+  return 'bug!';
+}
+
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numbers) {
+  let words = [];
+
+  for (let i in numbers) {
+    if (i) {
+      words.push(wordReplacer(numbers[i]));
+    }
+  }
+
+  return words;
+}
+
+fizzBuzz([1, 2, 3, 4, 5, 6]);
+
+function encodeChar(char) {
+  switch (char) {
+    case 'a':
+      return '1';
+    case 'e':
+      return '2';
+    case 'i':
+      return '3';
+    case 'o':
+      return '4';
+    case 'u':
+      return '5';
+    default:
+      return char;
+  }
 }
 
 // Desafio 9
-function encode() {
+function encode(string) {
   // seu código aqui
+  let encoded = [];
+
+  for (const index in string) {
+    if (string[index]) {
+      encoded.push(encodeChar(string[index]));
+    }
+  }
+
+  return encoded.join('');
 }
-function decode() {
+
+function decodeChar(char) {
+  switch (char) {
+    case '1':
+      return 'a';
+    case '2':
+      return 'e';
+    case '3':
+      return 'i';
+    case '4':
+      return 'o';
+    case '5':
+      return 'u';
+    default:
+      return char;
+  }
+}
+
+function decode(string) {
   // seu código aqui
+  let decoded = [];
+
+  for (const index in string) {
+    if (string[index]) {
+      decoded.push(decodeChar(string[index]));
+    }
+  }
+
+  return decoded.join('');
 }
 
 // Desafio 10
@@ -66,7 +144,6 @@ function hydrate() {
   // seu código aqui
 }
 
-
 module.exports = {
   calcArea,
   catAndMouse,
@@ -82,4 +159,4 @@ module.exports = {
   hydrate,
   splitSentence,
   triangleCheck,
-}
+};
