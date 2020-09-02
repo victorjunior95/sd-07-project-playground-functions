5d6f8f05bd0c0a978ad39b3a7a22d384d69ecc1d
Thays Costa <thayscosta3@gmail.com>
2020-09-02 13:51:34 -0300

    Commit inicial do Project Playground

--- src/challenges.js
+++ src/challenges.js
@@ -1,85 +1,90 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(boolean1, boolean2) {
+  if (boolean1 === true && boolean2 === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
+console.log(compareTrue(true, false));
 
-// Desafio 2
-function calcArea() {
-  // seu código aqui
-}
+// // Desafio 2
+// function calcArea() {
+//   // seu código aqui
+// }
 
-// Desafio 3
-function splitSentence() {
-  // seu código aqui
-}
+// // Desafio 3
+// function splitSentence() {
+//   // seu código aqui
+// }
 
-// Desafio 4
-function concatName() {
-  // seu código aqui
-}
+// // Desafio 4
+// function concatName() {
+//   // seu código aqui
+// }
 
-// Desafio 5
-function footballPoints() {
-  // seu código aqui
-}
+// // Desafio 5
+// function footballPoints() {
+//   // seu código aqui
+// }
 
-// Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+// // Desafio 6
+// function highestCount() {
+//   // seu código aqui
+// }
 
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+// // Desafio 7
+// function catAndMouse() {
+//   // seu código aqui
+// }
 
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+// // Desafio 8
+// function fizzBuzz() {
+//   // seu código aqui
+// }
 
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
+// // Desafio 9
+// function encode() {
+//   // seu código aqui
+// }
+// function decode() {
+//   // seu código aqui
+// }
 
-// Desafio 10
-function techList() {
-  // seu código aqui
-}
+// // Desafio 10
+// function techList() {
+//   // seu código aqui
+// }
 
-// Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+// // Desafio 11
+// function generatePhoneNumber() {
+//   // seu código aqui
+// }
 
-// Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+// // Desafio 12
+// function triangleCheck() {
+//   // seu código aqui
+// }
 
-// Desafio 13
-function hydrate() {
-  // seu código aqui
-}
+// // Desafio 13
+// function hydrate() {
+//   // seu código aqui
+// }
 
 
-module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  generatePhoneNumber,
-  techList,
-  highestCount,
-  hydrate,
-  splitSentence,
-  triangleCheck,
-}
+// module.exports = {
+//   calcArea,
+//   catAndMouse,
+//   compareTrue,
+//   concatName,
+//   decode,
+//   encode,
+//   fizzBuzz,
+//   footballPoints,
+//   generatePhoneNumber,
+//   techList,
+//   highestCount,
+//   hydrate,
+//   splitSentence,
+//   triangleCheck,
+// }
