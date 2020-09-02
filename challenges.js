115d3289dd1fdd0f50eb7e4c13d7f9840cfb10e1
Jorge Alves <alvesdjorge@gmail.com>
2020-09-02 17:59:01 -0300

    exercicios 1 2 e 3

--- challenges.js
+++ challenges.js
@@ -1,1 +1,22 @@
 // teste
+
+/* // Desafio 1
+function compareTrue(a, b) {
+ if (a == true && b == true) {
+    return(true)
+ } else {
+    return(false)
+ }
+}
+ */
+// Desafio 2
+
+let = a = 20;
+let = b = 40;
+
+function calcArea(base, height) {
+    aux = base*height/ 2
+    return(aux)
+}
+
+calcArea(a, b);
