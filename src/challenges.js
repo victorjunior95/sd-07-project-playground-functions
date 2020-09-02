3cac4e1512b2b09c9026bcbb749c99501d75f9dc
Nicholas Torres J. Vasconcelos <nicolastorresjatoba@gmail.com>
2020-09-02 19:18:46 -0300

    Desafios 7, 8, 9 e 10 respondidos

--- src/challenges.js
+++ src/challenges.js
@@ -12,7 +12,7 @@ function calcArea(base, height) {
 function splitSentence(word) {
   let arrayWord = [""];
   let x = 0;
-  for(let i = 0; i < word.length; i++){
+  for(let i = 0; i < word.length; i += 1){
     if (word[i] != " "){
       arrayWord[x] += word[i];
     }else {
@@ -38,7 +38,7 @@ function footballPoints(wins, ties) {
 function highestCount(array) {
   let highest = Math.max(...array);
   let repetition = 0;
-  for (let i = 0; i < array.length; i++){
+  for (let i = 0; i < array.length; i += 1){
     if (array[i] === highest){
       repetition += 1;
     }
@@ -48,25 +48,99 @@ function highestCount(array) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  // seu código aqui
+  let distCat1 = Math.abs(mouse-cat1);
+  let distCat2 = Math.abs(mouse-cat2);
+  if(distCat1 < distCat2){
+    return "cat1";
+  }else if(distCat2 === distCat1){
+    return "os gatos trombam e o rato foge";
+  }
+  else{
+    return "cat2";
+  }
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let phraseFizzBuzz = [];
+  for (let x = 0; x < array.length; x += 1){
+    if((array[x] % 3 === 0)&&(array[x] % 5 === 0)){
+      phraseFizzBuzz.push("fizzbuzz");
+    }else if(array[x] % 3 === 0){
+      phraseFizzBuzz.push("fizz");
+    }else if(array[x] % 5 === 0){
+      phraseFizzBuzz.push("buzz");
+    }else{
+      phraseFizzBuzz.push("bug!");
+      }
+  }
+  return phraseFizzBuzz;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(crypt) {
+  let crypted = "";
+  for(let x = 0;x < crypt.length; x += 1){
+    switch(crypt[x]){
+      case 'a':
+        crypted += '1';
+        break;
+      case 'e':
+        crypted += '2';
+        break;
+      case 'i':
+        crypted += '3';
+        break;
+      case 'o':
+        crypted[x] += '4';
+        break;
+      case 'u':
+        crypted[x] += '5';
+        break;
+      default:
+        crypted += crypt[x];
+        break;
+    }
+  }
+  return crypted;
 }
-function decode() {
-  // seu código aqui
+function decode(crypt) {
+  let crypted = "";
+  for(let x = 0;x < crypt.length; x += 1){
+    switch(crypt[x]){
+      case '1':
+        crypted += 'a';
+        break;
+      case '2':
+        crypted += 'e';
+        break;
+      case '3':
+        crypted += 'i';
+        break;
+      case '4':
+        crypted += 'o';
+        break;
+      case '5':
+        crypted += 'u';
+        break;
+      default:
+        crypted += crypt[x];
+        break;
+    }
+  }
+  return crypted;
 }
 
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  let objectTech = [];
+  for(let x = 0; x < array.length; x += 1){
+    objectTech.push({
+      tech: array[x],
+      name: name
+    });
+  }
+  return objectTech;
 }
 
 // Desafio 11
