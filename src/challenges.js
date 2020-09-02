aa46c526be8cb6c06a4d09b00152f127ce5a6ee5
hvenses <hvenses@gmail.com>
2020-09-02 15:16:12 -0300

    Comitando exercicio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(a, b) {
+  if (a == true && b == true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
@@ -66,7 +70,6 @@ function hydrate() {
   // seu código aqui
 }
 
-
 module.exports = {
   calcArea,
   catAndMouse,
@@ -82,4 +85,4 @@ module.exports = {
   hydrate,
   splitSentence,
   triangleCheck,
-}
+};
