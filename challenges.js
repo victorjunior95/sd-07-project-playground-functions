3cb24b9b2957bdeee7041f32f09908b6cde6a81b
Wendrick <wendrick.b.c1@gmail.com>
2020-09-02 12:41:52 -0300

    3 primeiras partes

--- challenges.js
+++ challenges.js
@@ -1,12 +1,30 @@
 function compareTrue(a, b) {
-    if (a === true && b === true) {
-        return true;
-    } else {
-        return false;
-    }
+  if (a === true && b === true) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function calcArea(base, height) {
+  return ((base * height) / 2);
+}
+
+function splitSentence(sentença) {
+  let splitedSentece = [];
+  sentença = sentença.split(" ");
+  for (const i of sentença) {
+    splitedSentece.push(i);
+  }
+  return splitedSentece;
 }
 
 let a = true;
 let b = true;
+let num1 = 10;
+let num2 = 5;
+let sentença = "Olá, hoje está um belo dia.";
 
 console.log(compareTrue(a, b));
+console.log(calcArea(num1, num2));
+console.log(splitSentence(sentença));
