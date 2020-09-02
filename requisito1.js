65ad195833f6eaa588ae5d840de8e1313666b778
vini-lima <vini20.henrique@gmail.com>
2020-09-02 13:59:45 -0300

    1. Function &&

---
+++ requisito1.js
@@ -0,0 +1,10 @@
+function compareTrue(a,b) {
+    let resultado;
+
+    if (a === b && a === true) {
+        resultado = true;
+    } else {
+        resultado = false;
+    }
+    return resultado;
+}
