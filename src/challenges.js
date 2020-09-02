fe62db1332fc0baa61fb4a4d676d5830895db4b5
Berilo Wanderley <wberilo@gmail.com>
2020-09-02 16:56:39 -0300

    bonus 1 e 3

--- src/challenges.js
+++ src/challenges.js
@@ -105,12 +105,28 @@ function techList(arr, name) {
   }
   return out;
 }
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Lucas'))
 
 // Desafio bonus 1
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arr) {
+  let c = [0,0,0,0,0,0,0,0,0];
+  if(arr.length > 11){
+    return 'Array com tamanho incorreto.';
+  }
+  let n = 0;
+  for(let i in arr){
+    if(arr[i]<0 || arr[i]>9){
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    c[arr[i]]+=1;
+  }
+  for(let i in c){
+    if(c[i]>2){
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  return `(${arr[0]}${arr[1]}) ${arr[2]}${arr[3]}${arr[4]}${arr[5]}${arr[6]}-${arr[7]}${arr[8]}${arr[9]}${arr[10]}`
 }
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 
 // Desafio bonus 2
 function triangleCheck() {
@@ -118,8 +134,17 @@ function triangleCheck() {
 }
 
 // Desafio bonus 3
-function hydrate() {
-  // seu código aqui
+function hydrate(str) {
+  num = ['1','2','3','4','5','6','7','8','9']
+  out = 0;
+  for(let i in str){
+    for(let j in num){
+      if(str[i]==num[j]){
+        out += parseInt(num[j])
+      }
+    }
+  }
+  return `${out} copos de água`;
 }
 
 module.exports = {
