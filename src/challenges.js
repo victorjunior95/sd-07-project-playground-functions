6648f050341690dc0be920890a624f83e0dd732e
moacyrrln <moacyrrln@gmail.com>
2020-09-02 16:39:13 -0300

    1639

--- src/challenges.js
+++ src/challenges.js
@@ -24,17 +24,59 @@ function footballPoints(wins, ties) {
 }
 // Desafio 6
 function highestCount(array) {
-  aux=array.sort(function(a, b){return a-b}).slice;
+  let aux=array.sort(function(a, b){return a-b}).slice;
+  let repete=1;
+  for (i=aux.length-2;i>0=i--)
+  {
+    if (aux[i]=array[array.length-1])
+    {
+      repete++;
+    }
+  }
+  return repete;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  d1=Math.abs(cat1-mouse);
+  d2=Math.abs(cat2-mouse);
+  if (d1==d2)
+  {
+    return "os gatos trombam e o rato foge";
+  }
+  if (d1<d2)
+  {
+    return "cat2";
+  }
+  if (d2<d1)
+  {
+    return "cat1";
+  }
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let retorno=[];
+  for (i in array)
+  {
+    if (array[i] % 3 == 0 || array[i] % 5 != 0)
+    {
+      retorno.push("fizz");
+    }
+    if (array[i] % 3 != 0 || array[i] % 5 == 0)
+    {
+      retorno.push("buzz");
+    }
+    if (array[i] % 3 == 0 || array[i] % 5 == 0)
+    {
+      retorno.push("fizzBuzz");
+    }
+    if (array[i] % 3 != 0 || array[i] % 5 != 0)
+    {
+      retorno.push("bug!");
+    }
+  }
+  return retorno;
 }
 
 // Desafio 9
