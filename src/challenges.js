aed5fac0bc7d0d9ac600f055f7952afe2a786381
renatomak <renato.mark.silva@gmail.com>
2020-09-02 16:11:19 -0300

    Correção de erro no Desafio 6

--- src/challenges.js
+++ src/challenges.js
@@ -33,34 +33,28 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
+function maiorValor(valores) {
+  let maior = 0;
+  for (valor of valores) {
+      if (valor > maior) {
+          maior = valor;
+      }
+  }
+
+  return maior;
+}
+
 function highestCount(vetor) {
-  let maiorRepeticao = 0;
-    let objMaior = {};
+  let maior = maiorValor(vetor);
+  let contaMaior = 0;
+  
+  for(const key in vetor){
+      if(maior === vetor[key]){
+          contaMaior += 1;
+      }
+  }
 
-    /**Monta um objeto com os valores do array inicial sendo a chave 
-     * do objeto. Dentro do segundo for passo a contar quantas vezes cada 
-     * chave se repete e sigo guardando esse resultado dentro do atributo 
-     * da chave
-     */
-    for (const i in vetor) {
-        objMaior[vetor[i]] = 0;
-
-        for (const j in vetor) {
-            if (vetor[i] === vetor[j]) {
-                objMaior[vetor[i]] += 1;
-            }
-        }
-    }
-
-    maiorRepeticao = objMaior[2];
-    for (const key in objMaior) {
-        if (maiorRepeticao < objMaior[key]) {
-            maiorRepeticao = objMaior[key];
-        }
-    }
-
-
-    return maiorRepeticao;
+  return contaMaior;
 }
 
 // Desafio 7
@@ -100,7 +94,7 @@ function techList() {
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
-}
+}    
 
 // Desafio 12
 function triangleCheck() {
