781ff24d21882bf91bf26ada0caab909d81f1e4e
Luise Rios <luisemiranda@gmail.com>
2020-09-02 15:51:55 -0300

    1 a 4 testando

--- src/challenges.js
+++ src/challenges.js
@@ -1,38 +1,42 @@
 // Desafio 1
 function compareTrue(valor1,valor2) {
-  
-  if (valor1 && valor2) {
-    return true;
+  let retorno = false;
+
+  if (valor1 === true && valor2 === true) {
+    retorno = true;
   } else {
-    return false;
+    retorno = false;
   }
- 
+  return retorno;
 }
-console.log(compareTrue())
 
 
 // Desafio 2
 function calcArea(base,height) {
- 
-  let area = (base*height/2);
+  let area = (base * height / 2);
 
   return area;
 }
+console.log(calcArea(base,height))
 
 // Desafio 3
 function splitSentence(umaString) {
-  
+    
   umArray = umaString.split(" ")
 
   return umArray;
 }
 
-console.log(splitSentence(umaString))
 
 // Desafio 4
-function concatName() {
+function concatName(arrayDeStrings) {
   
+  let stringFinal = (arrayDeStrings[(arrayDeStrings.length-1)] +" , "+ arrayDeStrings[0])
+  
+  return stringFinal;
 }
+
+
 
 // Desafio 5
 function footballPoints() {
