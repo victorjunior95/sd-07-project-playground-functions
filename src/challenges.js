cc30b692220bde246b54fea747573b98ba89dcb3
kramersilva <kramerscs@gmail.com>
2020-09-02 11:49:19 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,12 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(number1, number2) {
+  if (number1 == true && number2 == true) {
+    return true
+  } else {
+    return false
+  }
 }
+console.log(compareTrue(true,true))
 
 // Desafio 2
 function calcArea() {
