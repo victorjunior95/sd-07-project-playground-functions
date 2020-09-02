228a7592a20563847acb5e04f7a60617c2600766
Sebastiao Amaral <sebpiloto@gmail.com>
2020-09-02 16:45:40 -0300

    Corrigido desafio catAndMouse

--- src/challenges.js
+++ src/challenges.js
@@ -45,10 +45,10 @@ function highestCount() {
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
   if (mouse - cat1 < mouse - cat2) {
-    return cat1
+    return "cat1"
   }
   else if (mouse - cat1 > mouse - cat2) {
-    return cat2
+    return "cat2"
   } 
   else {
     return "os gatos trombam e o rato foge"
