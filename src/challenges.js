bd6cac30c59d71cba7d2cb500e5988c94b6c7a5d
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-09-02 13:50:00 -0300

    Conclusão desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,85 +1,94 @@
 // Desafio 1
 function compareTrue() {
-  // seu código aqui
-}
+    let aprovacao, formacao = true;
+    if (aprovacao == true && formacao == true) {
 
+        let felicidade = true;
+        return felicidade;
+    } else {
+
+        let felicidade = false;
+        return felicidade;
+    }
+}
 // Desafio 2
 function calcArea() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 3
 function splitSentence() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 4
 function concatName() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 5
 function footballPoints() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 6
 function highestCount() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 7
 function catAndMouse() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 8
 function fizzBuzz() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 9
 function encode() {
-  // seu código aqui
+    // seu código aqui
 }
+
 function decode() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 10
 function techList() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 11
 function generatePhoneNumber() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 12
 function triangleCheck() {
-  // seu código aqui
+    // seu código aqui
 }
 
 // Desafio 13
 function hydrate() {
-  // seu código aqui
+    // seu código aqui
 }
 
 
 module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  generatePhoneNumber,
-  techList,
-  highestCount,
-  hydrate,
-  splitSentence,
-  triangleCheck,
+    calcArea,
+    catAndMouse,
+    compareTrue,
+    concatName,
+    decode,
+    encode,
+    fizzBuzz,
+    footballPoints,
+    generatePhoneNumber,
+    techList,
+    highestCount,
+    hydrate,
+    splitSentence,
+    triangleCheck,
 }
