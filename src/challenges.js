b7acf4065e1a055cd5394abd386da0fbce2c1e3a
Vitor Rodrigues <vitor_rc@outlook.com>
2020-09-02 15:55:21 -0300

    Primeiros exercícios

--- src/challenges.js
+++ src/challenges.js
@@ -1,36 +1,81 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui .
+function compareTrue(value1, value2) {
+  // seu código aqui
+  if (value1 == true && value2 == true){
+    return true;
+  }
+  return false;
+
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  return (base*height)/2;
+
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(string) {
   // seu código aqui
+  return string.split(" ");
+
 }
 
 // Desafio 4
-function concatName() {
+function concatName(stringArray) {
   // seu código aqui
+  return `${stringArray[stringArray.length - 1]}, ${stringArray[0]}}`;
+
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  return (wins*3 + ties*1);
+
 }
 
 // Desafio 6
-function highestCount() {
+function highestCount(Array) {
   // seu código aqui
+  let uniqueKeys = Array.keys()
+  let index = {};
+  let greaterValue = 0;
+  for (let key of uniqueKeys) {
+      let cont = 0;
+      for(let chaves in Array){
+          if(Array[chaves] == key){
+              cont += 1;
+
+          }
+      }
+      if(cont>greaterValue){
+          greaterValue =  cont;
+
+      }
+
+      index[key]=cont;
+
+  }
+  return console.log(greaterValue);
+
 }
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  if(cat1 > cat2){
+    return cat2;
+
+  }
+  else if (cat2 > cat1){
+    return cat1;
+  }
+  else {
+    return 'os gatos trombam e o rato foge';
+  }
+
 }
 
 // Desafio 8
