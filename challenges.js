43d32ad8517dfbe1e1a4d57f7639483f5a151986
Wendrick <wendrick.b.c1@gmail.com>
2020-09-02 12:25:29 -0300

    Testando commits

---
+++ challenges.js
@@ -0,0 +1,12 @@
+function compareTrue(a, b) {
+    if (a === true && b === true) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let a = true;
+let b = true;
+
+console.log(compareTrue(a, b));
