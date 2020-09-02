1d8017bebc91d9732e135ecb1664e0dccc0ed20c
Thays Costa <thayscosta3@gmail.com>
2020-09-02 15:51:27 -0300

    Requisito obrigatório 4

--- src/challenges.js
+++ src/challenges.js
@@ -22,10 +22,10 @@ function splitSentence(string) {
 console.log(splitSentence("go Trybe"));
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  return array[array.length - 1] + ", " + array[0];
 }
-
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
 // Desafio 5
 function footballPoints() {
   // seu código aqui
