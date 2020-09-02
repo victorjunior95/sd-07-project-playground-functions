ce55cb8f8ef79d6eedd8a971351654b0be102f4d
Victor Junior <victorjr.muchacho@gmail.com>
2020-09-02 13:54:14 -0400

    resolvendo o desafio 2

--- src/challenges.js
+++ src/challenges.js
@@ -6,20 +6,15 @@ Considerando isso, crie uma função chamada compareTrue que, ao receber dois bo
 Retorne true se ambos os valores são verdadeiros;
 Retorne false se um ou ambos os parâmetros forem falsos.
 Faça a função utilizando o operador &&.*/
-let a = true;
-let b = false;
+// let a = true;
+// let b = true;
 
 function compareTrue(a, b) {
-  // let comparation = false;
-
   if (a === true && b === true) {
-    // comparation = true;
     console.log(true);
   } else {
     console.log(false);
   }
-
-  // return comparation;
 };
 
 compareTrue(a, b);
@@ -27,83 +22,89 @@ compareTrue(a, b);
 // Desafio 2
 /*Escreva uma função com o nome calcArea que receba um valor de base (chamado base) e outro de altura (chamado height) de um triângulo e retorne o cálculo da sua área.
 
-Lembre-se que a área de um triângulo é calculada através da seguinte fórmula: (base * altura) / 2.
-function calcArea() {
-  // seu código aqui
-}
+Lembre-se que a área de um triângulo é calculada através da seguinte fórmula: (base * altura) / 2.*/
+let base
+
+
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  console.log(area);
+};
+
+calcArea(base, height);
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
-}
+// function splitSentence() {
+//   // seu código aqui
+// }
 
-// Desafio 4
-function concatName() {
-  // seu código aqui
-}
+// // Desafio 4
+// function concatName() {
+//   // seu código aqui
+// }
 
-// Desafio 5
-function footballPoints() {
-  // seu código aqui
-}
+// // Desafio 5
+// function footballPoints() {
+//   // seu código aqui
+// }
 
-// Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+// // Desafio 6
+// function highestCount() {
+//   // seu código aqui
+// }
 
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+// // Desafio 7
+// function catAndMouse() {
+//   // seu código aqui
+// }
 
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+// // Desafio 8
+// function fizzBuzz() {
+//   // seu código aqui
+// }
 
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
+// // Desafio 9
+// function encode() {
+//   // seu código aqui
+// }
+// function decode() {
+//   // seu código aqui
+// }
 
-// Desafio 10
-function techList() {
-  // seu código aqui
-}
+// // Desafio 10
+// function techList() {
+//   // seu código aqui
+// }
 
-// Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+// // Desafio 11
+// function generatePhoneNumber() {
+//   // seu código aqui
+// }
 
-// Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+// // Desafio 12
+// function triangleCheck() {
+//   // seu código aqui
+// }
 
-// Desafio 13
-function hydrate() {
-  // seu código aqui
-}
+// // Desafio 13
+// function hydrate() {
+//   // seu código aqui
+// }
 
 
-module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  generatePhoneNumber,
-  techList,
-  highestCount,
-  hydrate,
-  splitSentence,
-  triangleCheck,
-}*/
+// module.exports = {
+//   calcArea,
+//   catAndMouse,
+//   compareTrue,
+//   concatName,
+//   decode,
+//   encode,
+//   fizzBuzz,
+//   footballPoints,
+//   generatePhoneNumber,
+//   techList,
+//   highestCount,
+//   hydrate,
+//   splitSentence,
+//   triangleCheck,
+// }
