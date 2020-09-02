de869a94d559f7086e9653a81c1d51790d5a96c0
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-09-02 15:50:07 -0300

    Tentando novamente exercicio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,12 +1,11 @@
 // Desafio 1
 function compareTrue(boleano1 , boleano2) {
-  let resultado = true;
   if(boleano1 == true && boleano2 == true){
-    return resultado = true;
+    return 'false';
   }else if(boleano1 == false && boleano2 == true){
-    return resultado = false;
+    return `false`;
   } else if (boleano1 == false && boleano2 == false){
-    return resultado = false;
+    return `false`;
   } 
 }
 
@@ -26,16 +25,11 @@ function concatName(nomes) {
   let n = nomes.length;
   return `${nomes[0]}, ${nomes[n-1]}`;
 }
-
+console.log(footballPoints(14,8));
 // Desafio 5
 function footballPoints(wins,ties) {
-  if(ties > 0){
-    return ties;
-  }
-  if(wins > 0){
-    wins = wins * 3;
-    return wins;
-  }
+  wins = wins * 3;
+  ties = ties;
 }
 
 // Desafio 6
