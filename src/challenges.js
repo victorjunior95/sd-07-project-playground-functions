ea89be742269725d1e4d84004be5b7a8ac1413b0
Muniz <vinicius.muniz.marin@gmail.com>
2020-09-02 19:11:57 -0300

    desafio 6 foi arrumado uma função que apresentava falhas

--- src/challenges.js
+++ src/challenges.js
@@ -61,7 +61,7 @@ function highestCount(arrayDeInteiros) {
   }
   function numeroRepeticoes(array, indice) {
     let numeroRepeticoes = 0;
-    for (let i = 0; i < array.length - 1; i += 1) {
+    for (let i = 0; i < array.length; i += 1) {
       if (array[indice] === array[i]) {
         numeroRepeticoes += 1;
       }
