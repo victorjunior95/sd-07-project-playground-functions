4596311a4fad3ab1db79a1bf2d74a038912acac1
spawnred <bruno.spawnhell@gmail.com>
2020-09-02 16:38:48 -0300

    incluindo item 5

--- src/challenges.js
+++ src/challenges.js
@@ -27,8 +27,8 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins,ties) {
+  return footballPoints = (wins*3+ties)
 }
 
 // Desafio 6
