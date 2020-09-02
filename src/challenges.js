6daab5b46e2a58dd051239922c39c3cbbaa12305
Camila Valentim <camilaifpb@hotmail.com>
2020-09-02 14:53:27 -0300

    operador &&

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,12 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(valor1, valor2) {
+  if (valor1 == 1 && valor2 == 2) {
+    return true;
+
+  } else {
+    return false;
+
+  }
 }
 
 // Desafio 2
