4504599cb3e30358cf915f7d1586ce364d6c14a5
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-09-02 13:59:37 -0400

    correção na função que verifica se 2 valores são verdadeiros

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /* 1 - Usando o operador &&
-JavaScript possui um operador lógico &&, o qual recebe dois valores e retorna true se ambos os valores são verdadeiros, e retorna false se algum dos valores não o for.
+JavaScript possui um operador lógico &&, o qual recebe dois valores e retorna
+true se ambos os valores são verdadeiros, e retorna false se algum dos valores não o for.
 
 Considerando isso, crie uma função chamada compareTrue que, ao receber dois booleanos:
 
@@ -7,10 +8,15 @@ Retorne true se ambos os valores são verdadeiros;
 Retorne false se um ou ambos os parâmetros forem falsos.
 Faça a função utilizando o operador &&. */
 
-function compareTrue(bool1, bool2){
-    if(bool1 && bool2){
-        return true;
-    } else {
-        return false;
-    }
+let opcao1 = true;
+let opcao2 = true;
+
+function compareTrue(valor1, valor2) {
+  if (valor1 && valor2 == true) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+console.log(compareTrue(opcao1, opcao2));
