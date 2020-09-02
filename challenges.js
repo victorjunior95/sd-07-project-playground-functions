9609896268eef2b945ac528dca2cf558dfbda912
Cainan Coutinho <cainan6697@gmail.com>
2020-09-02 15:27:05 -0300

    Segunda tentativa de validar o desafio 1

--- challenges.js
+++ challenges.js
@@ -1,71 +1,8 @@
 // Desafio 1
 function compareTrue(valor, valor1) {
-  if (valor === true && valor1 === true) {
+  if (valor == true && valor1 == true) {
     return true;
   } else {
     return false;
   }
-}
-
-// Desafio 2
-function calcArea() {
-  
-}
-
-// Desafio 3
-function splitSentence() {
-  
-}
-
-// Desafio 4
-function concatName() {
-  
-}
-
-// Desafio 5
-function footballPoints() {
-  
-}
-
-// Desafio 6
-function highestCount() {
-  
-}
-
-// Desafio 7
-function catAndMouse() {
-  
-}
-
-// Desafio 8
-function fizzBuzz() {
-  
-}
-
-// Desafio 9
-function encode() {
-  
-}
-function decode() {
-  
-}
-
-// Desafio 10
-function techList() {
-  
-}
-
-// Desafio 11
-function generatePhoneNumber() {
-  
-}
-
-// Desafio 12
-function triangleCheck() {
-  
-}
-
-// Desafio 13
-function hydrate() {
-  
 }
