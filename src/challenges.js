1cf52f7c3bca367edfe0033f37f6059cc5137858
Anderson Santana <anderslash@gmail.com>
2020-09-02 16:11:37 -0300

    Correção do desafio 07

--- src/challenges.js
+++ src/challenges.js
@@ -50,10 +50,8 @@ function highestCount(array) {
 function catAndMouse(mouse, cat1, cat2) {
   if ((cat1 - mouse) < (cat2 - mouse)) {
     return "cat1";
-  } else if {
-    ((cat2 - mouse) < (cat1 - mouse)) {
-      return "cat2";
-    }
+  } else if ((cat2 - mouse) < (cat1 - mouse)) {
+     return "cat2";
   } else {
     return "os gatos trombam e o rato foge";
   }
