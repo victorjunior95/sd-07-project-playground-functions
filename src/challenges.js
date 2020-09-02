84be3d53eb96cce9d30f6076e83270e839eaf73e
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-09-02 15:10:35 -0300

    Arrumando desafio 01 e fazendo desafio 02

--- src/challenges.js
+++ src/challenges.js
@@ -13,68 +13,75 @@ function compareTrue(boolean1, boolean2) {
 // console.log(compareTrue(teste,teste2));
 // -------------------------------------------------------------------
 
-// // Desafio 2
-// function calcArea() {
-//   // seu código aqui
-// }
+// Desafio 2
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area;
+}
 
-// // Desafio 3
-// function splitSentence() {
-//   // seu código aqui
-// }
+// --------------------TESTE DESAFIO 2--------------------------------
+// let teste = 30;
+// let teste2= 10;
+// console.log(calcArea(teste,teste2));
+// -------------------------------------------------------------------
 
-// // Desafio 4
-// function concatName() {
-//   // seu código aqui
-// }
+// Desafio 3
+function splitSentence() {
+  // seu código aqui
+}
 
-// // Desafio 5
-// function footballPoints() {
-//   // seu código aqui
-// }
+// Desafio 4
+function concatName() {
+  // seu código aqui
+}
 
-// // Desafio 6
-// function highestCount() {
-//   // seu código aqui
-// }
+// Desafio 5
+function footballPoints() {
+  // seu código aqui
+}
 
-// // Desafio 7
-// function catAndMouse() {
-//   // seu código aqui
-// }
+// Desafio 6
+function highestCount() {
+  // seu código aqui
+}
 
-// // Desafio 8
-// function fizzBuzz() {
-//   // seu código aqui
-// }
+// Desafio 7
+function catAndMouse() {
+  // seu código aqui
+}
 
-// // Desafio 9
-// function encode() {
-//   // seu código aqui
-// }
-// function decode() {
-//   // seu código aqui
-// }
+// Desafio 8
+function fizzBuzz() {
+  // seu código aqui
+}
 
-// // Desafio 10
-// function techList() {
-//   // seu código aqui
-// }
+// Desafio 9
+function encode() {
+  // seu código aqui
+}
+function decode() {
+  // seu código aqui
+}
 
-// // Desafio 11
-// function generatePhoneNumber() {
-//   // seu código aqui
-// }
+// Desafio 10
+function techList() {
+  // seu código aqui
+}
 
-// // Desafio 12
-// function triangleCheck() {
-//   // seu código aqui
-// }
+// Desafio 11
+function generatePhoneNumber() {
+  // seu código aqui
+}
 
-// // Desafio 13
-// function hydrate() {
-//   // seu código aqui
-// }
+// Desafio 12
+function triangleCheck() {
+  // seu código aqui
+}
+
+// Desafio 13
+function hydrate() {
+  // seu código aqui
+}
 
 module.exports = {
   calcArea,
