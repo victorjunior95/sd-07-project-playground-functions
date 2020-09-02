db62c53ec60d2f5a6273c21e303bc0cb80c17cae
mayconfelipes <mayconfelipe.dev@gmail.com>
2020-09-02 18:46:36 -0300

    Requisito 2. VAMOS COM TUDO :rocket:

--- src/challenges.js
+++ src/challenges.js
@@ -7,8 +7,9 @@ function compareTrue(valor1,valor2) {
   }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base,height) {
+  area = (base * height) / 2;
+  return area;
 }
 
 // Desafio 3
