61e3613356f0e73880f229e07bee580bf05928cd
tiagorichter <t2engenharia@gmail.com>
2020-09-02 18:01:59 -0300

    Questão 02

--- src/challenges.js
+++ src/challenges.js
@@ -5,8 +5,9 @@ function compareTrue(valor1, valor2) {
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  return (base*height)/2;
 }
 
 // Desafio 3
