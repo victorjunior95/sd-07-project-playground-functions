f5f4b28e04c0b3faf5a1415b001c624c66e936ae
Leonardo <lecastro16@gmail.com>
2020-09-02 17:57:59 -0300

    exercicio 1 a 8

--- src/challenges.js
+++ src/challenges.js
@@ -17,6 +17,8 @@ function calcArea(base, height) {
 // Desafio 3
 function splitSentence(recebeString) {
   // seu código aqui
+  let stringA;
+
   return recebeString.split("");
 }
 
@@ -55,10 +57,10 @@ function highestCount(arrayDeNumeros) {
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
-  if (cat1 - mouse > cat2 - mouse && cat1 - mouse !== cat2 - mouse) {
-    return cat2;
-  } else if (cat1 - mouse < cat2 - mouse && cat1 - mouse !== cat2 - mouse) {
-    return cat1;
+  if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse) && Math.abs(cat1 - mouse) !== Math.abs(cat2 - mouse)) {
+    return "cat2";
+  } else if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse) && Math.abs(cat1 - mouse) !== Math.abs(cat2 - mouse)) {
+    return "cat1";
   } else {
     return "os gatos trombam e o rato foge";
   }
