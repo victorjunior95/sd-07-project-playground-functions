d64a2efe6a63565c0185ce73635e92d49d2dbf78
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-09-02 15:37:55 -0300

    correção desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,17 +1,9 @@
 // Desafio 1
 function compareTrue(valor1,valor2) {
   // seu código aqui
-  let comparador;
-  if(valor1 > 10 && valor2 < 10){
-    comparador = true;
-  }
-  else{
-    comparador = false;
-  }
-  
-  return comparador;
+  return valor1 && valor2;
 }
-
+console.log(compareTrue(true,true));
 
 // Desafio 2
 function calcArea() {
