881de4894a870675153039c18948a420f518fa80
moacyrrln <moacyrrln@gmail.com>
2020-09-02 18:19:20 -0300

    1819

--- src/challenges.js
+++ src/challenges.js
@@ -20,15 +20,15 @@ function concatName(array) {
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  return 3*wins,ties;
+  return 3*wins+ties;
 }
 // Desafio 6
 function highestCount(array) {
-  let aux=array.sort(function(a, b){return a-b}).slice;
+  let aux=array.sort(function(a, b){return a-b}).slice();
   let repete=1;
-  for (i=aux.length-2;i>0=i--)
+  for (i=aux.length-2;i>0;i--)
   {
-    if (aux[i]=array[array.length-1])
+    if (aux[i]==array[array.length-1])
     {
       repete++;
     }
@@ -46,32 +46,33 @@ function catAndMouse(mouse, cat1, cat2) {
   }
   if (d1<d2)
   {
-    return "cat2";
+    return "cat1";
   }
   if (d2<d1)
   {
-    return "cat1";
+    return "cat2";
   }
 }
 
 // Desafio 8
-function fizzBuzz(array) {
+function fizzBuzz(array)
+{
   let retorno=[];
   for (i in array)
   {
-    if (array[i] % 3 == 0 || array[i] % 5 != 0)
+    if (array[i] % 3 == 0 && array[i] % 5 != 0)
     {
       retorno.push("fizz");
     }
-    if (array[i] % 3 != 0 || array[i] % 5 == 0)
+    if (array[i] % 3 != 0 && array[i] % 5 == 0)
     {
       retorno.push("buzz");
     }
-    if (array[i] % 3 == 0 || array[i] % 5 == 0)
+    if (array[i] % 3 == 0 && array[i] % 5 == 0)
     {
       retorno.push("fizzBuzz");
     }
-    if (array[i] % 3 != 0 || array[i] % 5 != 0)
+    if (array[i] % 3 != 0 && array[i] % 5 != 0)
     {
       retorno.push("bug!");
     }
@@ -80,11 +81,41 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let aux=string.slice();
+  let vogaltonumber={
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  }
+  for (j=0;j<aux.length;j++)
+  {
+    for (i in vogaltonumber)
+    {
+      aux=aux.replace(i,vogaltonumber[i]);
+    }
+  }
+  return aux;
 }
-function decode() {
-  // seu código aqui
+function decode(string) {
+  let aux=string.slice();
+  let vogaltonumber={
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  }
+  for (j=0;j<aux.length;j++)
+  {
+    for (i in vogaltonumber)
+    {
+      aux=aux.replace(vogaltonumber[i],i);
+    }
+  }
+  return aux;
 }
 
 // Desafio 10
@@ -124,3 +155,20 @@ module.exports = {
   splitSentence,
   triangleCheck,
 } 
+
+
+
+
+console.log(compareTrue(true, true));
+console.log(calcArea(10, 20));
+console.log(splitSentence("qwe ert tyu cgh asdf asd sdzf"));
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
+console.log(footballPoints(100, 4));
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
+console.log(catAndMouse(-10, 11, 8));
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
+console.log(encode("hi there!"));
+console.log(decode("h3 th2r2!"));
+console.log();
+console.log();
+console.log();
