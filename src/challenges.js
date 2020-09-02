eec8f4b45a1b20a5885b6f6a0434d806b400e4b3
richardfazeredo <richardcompletetour@gmail.com>
2020-09-02 19:16:20 -0300

    Substituindo vogais por números e vice-versa.

--- src/challenges.js
+++ src/challenges.js
@@ -83,10 +83,21 @@ function fizzBuzz(numbers) {
 
 // Desafio 9
 function encode(strings) {
-
+  return strings
+  .replace(/a/g, '1')
+  .replace(/e/g, '2')
+  .replace(/i/g, '3')
+  .replace(/o/g, '4')
+  .replace(/u/g, '5')
 }
-function decode() {
-  // seu código aqui
+
+function decode(numbers) {
+  return numbers
+  .replace(/1/g, 'a')
+  .replace(/2/g, 'e')
+  .replace(/3/g, 'i')
+  .replace(/4/g, 'o')
+  .replace(/5/g, 'u')
 }
 
 // Desafio 10
