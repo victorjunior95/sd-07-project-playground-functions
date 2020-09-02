99ef7e39df06a8362431719154f68c2c4252b39d
marcelosoliveira <msbobsk8@gmail.com>
2020-09-02 19:08:41 -0300

    desafio 10 lista de tecnologias

--- src/challenges.js
+++ src/challenges.js
@@ -167,7 +167,7 @@ function techList(tech, name) {
   }
   return result;
 }
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
+
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
