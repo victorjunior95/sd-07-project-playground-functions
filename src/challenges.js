c4ee712bed1f7b87cb3b83c35fe0cde282cd8fab
FabricioS <fab.silva.x2@gmail.com>
2020-09-02 16:54:07 -0300

    4 - Concatenação de strings 5 - Pontos no futebol 6 - Repetição do maior número

--- src/challenges.js
+++ src/challenges.js
@@ -23,18 +23,33 @@ function splitSentence(separator) {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(arrayStrings) {
+  arrayStrings = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
+  let last = arrayStrings[arrayStrings.length - 1];
+  let first = arrayStrings[0] ;
+  return `${last}, ${first}`;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  wins = wins * 3 ;
+  ties = ties * 1 ;
+  let points = wins + ties ;
+  return points ;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(arrayNum) {
+  arrayNum = [9, 1, 2, 3, 9, 5, 7];
+  let highest = 0;
+  let highestArrayNum = 0;
+  for ( i in arrayNum ){
+    if ( arrayNum[i] >= highest ){
+      highest = arrayNum[i];
+      highestArrayNum = highestArrayNum + 1;
+    };
+  };
+  return highestArrayNum;
 }
 
 // Desafio 7
