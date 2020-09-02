50170cf40e935b2026840f15b10093cf08e654c1
Muniz <vinicius.muniz.marin@gmail.com>
2020-09-02 16:10:27 -0300

    desafio 2 feito utilizando calculo matematico

--- src/challenges.js
+++ src/challenges.js
@@ -11,8 +11,9 @@ function compareTrue(primeiroValor, segundoValor) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = base * height / 2;
+  return area;
 }
 
 // Desafio 3
