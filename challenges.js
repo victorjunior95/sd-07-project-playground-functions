99d183017c18697b35200aed95144e34bf6a9134
Rodolfo <rodolfojr94@gmail.com>
2020-09-02 11:56:19 -0300

    Feita 1. Testando...

---
+++ challenges.js
@@ -0,0 +1,13 @@
+/*1 - Usando o operador &&
+JavaScript possui um operador lógico &&, o qual recebe dois valores e retorna true se ambos os valores são verdadeiros, e retorna false se algum dos valores não o for.
+
+Considerando isso, crie uma função chamada compareTrue que, ao receber dois booleanos:
+
+Retorne true se ambos os valores são verdadeiros;
+Retorne false se um ou ambos os parâmetros forem falsos.
+Faça a função utilizando o operador &&. */
+function compareTrue(a , b){
+
+    a === true && b === true ? true : false ;
+
+};
