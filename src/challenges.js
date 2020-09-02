00b95bc4951567dbcabb93b87e58f4b0d0102414
moraisgabri <gabriel8azevedo@gmail.com>
2020-09-02 16:48:45 -0300

    desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -46,8 +46,14 @@ function highestCount(array) {
 
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if ((mouse - cat1) > (mouse - cat2)) {
+    return cat2
+  } else if ((mouse - cat1) < (mouse - cat2)) {
+    return cat1
+  } else if (cat1 === cat2) {
+    return "os gatos trombam e o rato foge"
+  }
 }
 
 // Desafio 8
