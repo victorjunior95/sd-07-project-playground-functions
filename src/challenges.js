ab10c5f34825a5496f9594363ae4b87f53c52d0a
Arthur Nunes <coletivosuburbano@gmail.com>
2020-09-02 16:03:03 -0300

    ex 1 ao 6

--- src/challenges.js
+++ src/challenges.js
@@ -1,32 +1,60 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(a, b) {
+  if (a && b) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+console.log(compareTrue(true, true));
+console.log(compareTrue(true, false));
+console.log(compareTrue(false, true));
+console.log(compareTrue(false, false));
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
+
+console.log(calcArea(20, 2));
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split(' ');
 }
+
+console.log(splitSentence('O rato roeu a roupa do rei de roma!'));
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  return array.slice(-1) + ', ' + array[0];
 }
+
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return wins * 3 + ties;
 }
 
+console.log(footballPoints(10, 6));
+
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  let maiorNumero = 0;
+  let contador = 0;
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] >= maiorNumero) {
+      maiorNumero = array[i];
+      contador += 1;
+    }
+  }
+  return console.log(contador);
 }
+
+highestCount([9, 1, 2, 3, 9, 5, 7]);
 
 // Desafio 7
 function catAndMouse() {
@@ -42,6 +70,7 @@ function fizzBuzz() {
 function encode() {
   // seu código aqui
 }
+
 function decode() {
   // seu código aqui
 }
@@ -66,7 +95,6 @@ function hydrate() {
   // seu código aqui
 }
 
-
 module.exports = {
   calcArea,
   catAndMouse,
@@ -82,4 +110,4 @@ module.exports = {
   hydrate,
   splitSentence,
   triangleCheck,
-}
+};
