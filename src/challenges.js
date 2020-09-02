a656904ccd93f2bfed3e1de13edfb780e612cc54
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-09-02 15:39:30 -0300

    retorna pontuação de um time de futebol

--- src/challenges.js
+++ src/challenges.js
@@ -23,8 +23,8 @@ function concatName(arrayStrings) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + (ties * 1);
 }
 
 // Desafio 6
