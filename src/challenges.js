f5b946f587c4a5d3b01237d1c4117cab31bb3491
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-09-02 18:16:14 -0300

    desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -4,10 +4,10 @@ let b= false;
 function compareTrue(a, b) {
   // seu código aqui
   let resultado = a && b;
-  // return console.log(resultado);
+  // return console.log("resultado do desafio 1", resultado);
   return resultado;
 }
-compareTrue(a,b);
+// compareTrue(a,b);
 
 // Desafio 2
 let base= 1;
@@ -15,13 +15,13 @@ let height= 2;
 function calcArea(base, height) {
   // seu código aqui
   let area = base*height/2
-  // return console.log(area);
+  // return console.log("resultado do desafio 2", area, "m2");
   return area;
 }
 calcArea(base, height);
 
 // Desafio 3
-let string = "go Trybe"
+let string = "vamo que vam"
 let array = [];
 // console.log(string[6])
 // console.log(string.length)
@@ -35,26 +35,50 @@ let array = [];
 // console.log(string[i])
 // console.log(array);
 // console.log("---")
+//função split pode usar, você remove oq precisa e ele retorna separado e sem ele.
+// for (i=0; i<string.length; i += 1) {
+//   if (string[i] == " "){//completamente diferente de string[i] = " "!!!
+//   array = [string.slice(j,i),string.slice(i,string.length)];
+//   j=
+// }
+// }
+
 
 function splitSentence(string) {
   // seu código aqui
-  for (i=0; i<string.length; i += 1) {
-    if (string[i] == " "){//completamente diferente de string[i] = " "!!!
-      array = [string.slice(0,i),string.slice(i,string.length)];
-    }
-  }
-  return console.log(array);
+ let array = string.split(" ");
+  return console.log("resultado do desafio 3", array);
   return array;
 } 
-
-
 splitSentence(string);
 
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+let nomeCompleto = ['Vanessa', 'Pimentel', 'Lages', 'Martins', 'Ferreira'];
+let nomeEmIngles = "";
+// let ultimo = nomeCompleto.length;
+// console.log("resultado do desafio 3", ultimo);
+// nomeEmIngles = nomeCompleto[ultimo]+", "+ nomeCompleto[0];
+// nomeEmIngles = nomeCompleto[nomeCompleto.length] + nomeCompleto[0];
+for (i=0; i<nomeCompleto.length; i +=1){
+  if (i=nomeCompleto.length){
+    nomeEmIngles = nomeCompleto[i] +", "+ nomeCompleto[0];
+  }
 }
+// console.log("resultado do desafio 3", nomeEmIngles);
+
+
+function concatName(nomeCompleto) {
+  // seu código aqui
+  for (i=0; i<nomeCompleto.length; i +=1){
+    if (i=nomeCompleto.length){
+      nomeEmIngles = nomeCompleto[i] +", "+ nomeCompleto[0];
+    }
+  }
+  return console.log("resultado do desafio 3", nomeEmIngles);
+  return nomeEmIngles;
+}
+
 
 // Desafio 5
 function footballPoints() {
