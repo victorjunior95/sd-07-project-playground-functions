4e2c1314faf592c8784ed4cacaddbd327fdd65ec
Raphael Rodrigues <rapha.rodrigues04@gmail.com>
2020-09-02 17:36:18 -0300

    Testando a resolução do segundo exercicio

--- src/challenges.js
+++ src/challenges.js
@@ -7,11 +7,10 @@ function compareTrue(dormir, acordado) {
   }
 }
 
-console.log(compareTrue());
-
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base / height) / 2;
+  return area
 }
 
 // Desafio 3
