5bd48b8a564506194a8a1e269b5b7d60840ea421
marcelosoliveira <msbobsk8@gmail.com>
2020-09-02 15:20:35 -0300

    Desafio 3 Dividindo a frase

--- src/challenges.js
+++ src/challenges.js
@@ -11,8 +11,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(palavra) {
+  let splits = palavra.split(' ', 2);
+  return splits;
 }
 
 // Desafio 4
