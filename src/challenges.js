71793d1e4d00016225066c8409d97e50bee12a28
mayconfelipes <mayconfelipe.dev@gmail.com>
2020-09-02 18:33:26 -0300

    Requisito 1. VAMOS COM TUDO :rocket:

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,8 @@
 // Desafio 1
 function compareTrue(valor1,valor2) {
-  if (valor1 === true && valor2 === true){
+  if (valor1 && valor2){
     return true;
-  } else {
+  } 
     return false;
   }
 
