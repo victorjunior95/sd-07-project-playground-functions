ed0ca11340ac3acaadbb97b6747751d761252832
Thays Costa <thayscosta3@gmail.com>
2020-09-02 14:52:11 -0300

    Respondendo requisitos 1 e 2.

--- src/challenges.js
+++ src/challenges.js
@@ -6,85 +6,87 @@ function compareTrue(boolean1, boolean2) {
     return false;
   }
 }
-console.log(compareTrue(true, false));
+console.log(compareTrue(false, true));
 
-// // Desafio 2
-// function calcArea() {
-//   // seu código aqui
-// }
-
-// // Desafio 3
-// function splitSentence() {
-//   // seu código aqui
-// }
-
-// // Desafio 4
-// function concatName() {
-//   // seu código aqui
-// }
-
-// // Desafio 5
-// function footballPoints() {
-//   // seu código aqui
-// }
-
-// // Desafio 6
-// function highestCount() {
-//   // seu código aqui
-// }
-
-// // Desafio 7
-// function catAndMouse() {
-//   // seu código aqui
-// }
-
-// // Desafio 8
-// function fizzBuzz() {
-//   // seu código aqui
-// }
-
-// // Desafio 9
-// function encode() {
-//   // seu código aqui
-// }
-// function decode() {
-//   // seu código aqui
-// }
-
-// // Desafio 10
-// function techList() {
-//   // seu código aqui
-// }
-
-// // Desafio 11
-// function generatePhoneNumber() {
-//   // seu código aqui
-// }
-
-// // Desafio 12
-// function triangleCheck() {
-//   // seu código aqui
-// }
-
-// // Desafio 13
-// function hydrate() {
-//   // seu código aqui
-// }
+// Desafio 2
+function calcArea(base, height) {
+  return (base * height) / 2;
+}
+console.log(calcArea(15,5));
 
 
-// module.exports = {
-//   calcArea,
-//   catAndMouse,
-//   compareTrue,
-//   concatName,
-//   decode,
-//   encode,
-//   fizzBuzz,
-//   footballPoints,
-//   generatePhoneNumber,
-//   techList,
-//   highestCount,
-//   hydrate,
-//   splitSentence,
-//   triangleCheck,
-// }
+// Desafio 3
+function splitSentence() {
+  // seu código aqui
+}
+
+// Desafio 4
+function concatName() {
+  // seu código aqui
+}
+
+// Desafio 5
+function footballPoints() {
+  // seu código aqui
+}
+
+// Desafio 6
+function highestCount() {
+  // seu código aqui
+}
+
+// Desafio 7
+function catAndMouse() {
+  // seu código aqui
+}
+
+// Desafio 8
+function fizzBuzz() {
+  // seu código aqui
+}
+
+// Desafio 9
+function encode() {
+  // seu código aqui
+}
+function decode() {
+  // seu código aqui
+}
+
+// Desafio 10
+function techList() {
+  // seu código aqui
+}
+
+// Desafio 11
+function generatePhoneNumber() {
+  // seu código aqui
+}
+
+// Desafio 12
+function triangleCheck() {
+  // seu código aqui
+}
+
+// Desafio 13
+function hydrate() {
+  // seu código aqui
+}
+
+
+module.exports = {
+  calcArea,
+  catAndMouse,
+  compareTrue,
+  concatName,
+  decode,
+  encode,
+  fizzBuzz,
+  footballPoints,
+  generatePhoneNumber,
+  techList,
+  highestCount,
+  hydrate,
+  splitSentence,
+  triangleCheck,
+}
