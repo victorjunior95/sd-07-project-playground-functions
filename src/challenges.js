db1743a77c221ce7100eb5f2371e4dcd9a489881
Carol Bezerra <carol.arbe@gmail.com>
2020-09-02 15:01:17 -0300

    Initial commit

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(booleanA, booleanB) {
+  if(booleanA && booleanB) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
