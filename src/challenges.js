386b960c9e2430ba34968ee06c197d6521181b2c
Carol Bezerra <carol.arbe@gmail.com>
2020-09-02 17:01:07 -0300

    Requisito 7 e 9

--- src/challenges.js
+++ src/challenges.js
@@ -66,24 +66,26 @@ function highestCount(arrayNumbers) {
 function catAndMouse(mouse, cat1, cat2) {
   let result;
   if (cat1 - mouse < cat2 - mouse) {
-    result = "cat1";
-  } else if (cat2 - mouse < cat1 - mouse) {
-    result = "cat2";
+    result = 'cat1';
+  } 
+  if (cat2 - mouse < cat1 - mouse) {
+    result = 'cat2';
   } else {
     result = 'os gatos trombam e o rato foge';
   }
   return result;
 }
+console.log(catAndMouse(0, 3, 2));
 
 // Desafio 8
 function fizzBuzz(arrayNum) {
   let arrayFizzBuzz = [];
   for (let i = 0; i < arrayNum.length; i += 1) {
-    if (arrayNum[i] % 3 == 0 && arrayNum[i] % 5 == 0) {
+    if (arrayNum[i] % 3 === 0 && arrayNum[i] % 5 === 0) {
       arrayFizzBuzz.push('fizzBuzz');
-    } else if (arrayNum[i] % 3 == 0) {
+    } else if (arrayNum[i] % 3 === 0) {
       arrayFizzBuzz.push('fizz');
-    } else if (arrayNum[i] % 5 == 0) {
+    } else if (arrayNum[i] % 5 === 0) {
       arrayFizzBuzz.push('buzz');
     } else {
       arrayFizzBuzz.push('bug!');
@@ -93,12 +95,45 @@ function fizzBuzz(arrayNum) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(paramString) {
+  let newString = '';
+  for (let i = 0; i <= paramString.length-1; i += 1) {
+    if (paramString[i] === 'a') {
+      newString += '1';
+    } else if (paramString[i] === 'e') {
+      newString += '2';
+    } else if (paramString[i] === 'i') {
+      newString += '3';
+    } else if (paramString[i] === 'o') {
+      newString += '4';
+    } else if (paramString[i] === 'u') {
+      newString += '5';
+    } else {
+      newString += paramString[i];
+    }
+  }
+  return newString;
 }
-function decode() {
-  // seu código aqui
+function decode(paramString2) {
+  let newString2 = '';
+  for (let index = 0; index <= paramString2.length-1; index += 1) {
+    if (paramString2[index] === '1') {
+      newString2 += 'a';
+    } else if (paramString2[index] === '2') {
+      newString2 += 'e';
+    } else if (paramString2[index] === '3') {
+      newString2 += 'i';
+    } else if (paramString2[index] === '4') {
+      newString2 += 'o';
+    } else if (paramString2[index] === '5') {
+      newString2 += 'u';
+    } else {
+      newString2 += paramString2[index];
+    }
+  }
+  return newString2;
 }
+console.log(decode("h3 th2r2"));
 
 // Desafio 10
 function techList() {
