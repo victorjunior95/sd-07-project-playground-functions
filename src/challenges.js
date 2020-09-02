7bfc329c3639976b27a0f82438ea11893bdcdc37
Emanuelle Brasil <emanuelle.rbl@gmail.com>
2020-09-02 16:18:23 -0300

    Desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -84,11 +84,55 @@ function fizzBuzz(arrayNumbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let newString = "";
+    for(let i = 0; i < string.length; i += 1){
+        switch(string[i]){
+            case 'a':
+                newString += "1";
+                break;
+            case 'e':
+                newString += "2";
+                break;
+            case 'i':
+                newString += "3";
+                break; 
+            case 'o':
+                newString += "4";
+                break;
+            case 'u':
+                newString += "5";
+                break;
+            default:
+                newString += string[i];                   
+        }
+    }
+    return newString;
 }
-function decode() {
-  // seu código aqui
+function decode(string) {
+  let newString = "";
+    for(let i = 0; i < string.length; i += 1){
+        switch(string[i]){
+            case '1':
+                newString += "a";
+                break;
+            case '2':
+                newString += "e";
+                break;
+            case '3':
+                newString += "i";
+                break; 
+            case '4':
+                newString += "o";
+                break;
+            case '5':
+                newString += "u";
+                break;
+            default:
+                newString += string[i];                   
+        }
+    }
+    return newString;
 }
 
 // Desafio 10
