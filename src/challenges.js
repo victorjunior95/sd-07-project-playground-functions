65fcb35ee041a32a7eac94d8c8bb5cf1c1210ef0
Pedro MF <pedromarques1011@gmail.com>
2020-09-02 16:26:41 -0300

    mano?

--- src/challenges.js
+++ src/challenges.js
@@ -76,7 +76,7 @@ function fizzBuzz(array) {
 
 // Desafio 9
 function encode(frase) {
-  const list = ['a'=1, 'e'=2, 'i'=3, 'o'=4, 'u'=5];
+  const list = [a=1, e=2, i=3, o=4, u=5];
   let arrayfrase = frase.split('');
   for (const key in arrayfrase) {
     for (const keylist in list) {
@@ -88,7 +88,7 @@ function encode(frase) {
   return arrayfrase.join('');
 }
 function decode() {
-  const list = ['1'='a', '2'='e', '3'='i', '4'='o', '5'='u'];
+  const list = [1='a', 2='e', 3='i', 4='o', 5='u'];
   let arrayfrase = frase.split('');
   for (const key in arrayfrase) {
     for (const keylist in list) {
