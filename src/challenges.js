0a52e0b61a4cd44e5806aeab4297d323a5183ced
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-09-02 17:03:03 -0300

    desafio 7 - alterando return com string para return com parametro

--- src/challenges.js
+++ src/challenges.js
@@ -76,13 +76,13 @@ function catAndMouse(mouse, cat1, cat2) {
   let distanceCat2 = cat2 - mouse;
 
   if (distanceCat1 < distanceCat2) {
-    console.log("cat1");
+    return cat1;
   }
   else if (distanceCat2 < distanceCat1){
-    console.log("cat2");
+    return cat2;
   }
   else {
-    console.log("os gatos trombam e o rato foge");
+    return "os gatos trombam e o rato foge";
   }
 }
 
