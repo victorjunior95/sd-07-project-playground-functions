d35c8c3a84f3835c7e9e19cc9959f4ebf2f5903d
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-09-02 16:30:25 -0300

    Resolvido o Desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -45,9 +45,15 @@ function highestCount(array) {
 };
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+function catAndMouse(mouse, cat1, cat2) {
+  if (mouse - cat1 > mouse - cat2) {
+    return "cat2";
+  } else if (mouse - cat1 < mouse - cat2) {
+    return "cat1";
+  } else {
+    return "os gatos trombam e o rato foge"
+  }
+};
 
 // Desafio 8
 function fizzBuzz() {
