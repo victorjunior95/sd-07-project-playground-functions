ad50427d7f48447b33291bbc863e63d4b530a528
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-09-02 15:14:16 -0300

    Resolvido o Desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,11 @@
 // Desafio 1
-function compareTrue() {
-  
-}
+function compareTrue(boolean1, boolean2) {
+  if (boolean1 === true && boolean2 === true) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 // Desafio 2
 function calcArea() {
