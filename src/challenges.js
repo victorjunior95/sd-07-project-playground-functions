6a0735b549899461752978ebd764c15e95476659
renatomak <renato.mark.silva@gmail.com>
2020-09-02 16:28:55 -0300

    Resolução do Desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -36,9 +36,9 @@ function footballPoints(wins, ties) {
 function maiorValor(valores) {
   let maior = 0;
   for (valor of valores) {
-      if (valor > maior) {
-          maior = valor;
-      }
+    if (valor > maior) {
+      maior = valor;
+    }
   }
 
   return maior;
@@ -47,11 +47,11 @@ function maiorValor(valores) {
 function highestCount(vetor) {
   let maior = maiorValor(vetor);
   let contaMaior = 0;
-  
-  for(const key in vetor){
-      if(maior === vetor[key]){
-          contaMaior += 1;
-      }
+
+  for (const key in vetor) {
+    if (maior === vetor[key]) {
+      contaMaior += 1;
+    }
   }
 
   return contaMaior;
@@ -63,18 +63,31 @@ function catAndMouse(mouse, cat1, cat2) {
   cat1 = Math.abs(cat1 - mouse);
   cat2 = Math.abs(cat2 - mouse);
 
-  if(cat1 === cat2){
-      resultado = "os gatos trombam e o rato foge";
-  }else{
-      resultado = (cat1 > cat2) ? "cat2" : "cat1";
+  if (cat1 === cat2) {
+    resultado = "os gatos trombam e o rato foge";
+  } else {
+    resultado = (cat1 > cat2) ? "cat2" : "cat1";
   }
-  
+
   return resultado;
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let arrayRetorno = [];
+  for (const key in array) {
+    if (array[key] % 5 == 0 && array[key] % 3 == 0) {
+      arrayRetorno.push("fizzBuzz");
+    } else if (array[key] % 3 == 0) {
+      arrayRetorno.push("fizz");
+    } else if (array[key] % 5 == 0) {
+      arrayRetorno.push("buzz");
+    } else if (!(array[key] % 5 == 0 && array[key] % 3 == 0)) {
+      arrayRetorno.push("bug!");
+    }
+  }
+
+  return arrayRetorno;
 }
 
 // Desafio 9
@@ -94,7 +107,7 @@ function techList() {
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
-}    
+}
 
 // Desafio 12
 function triangleCheck() {
