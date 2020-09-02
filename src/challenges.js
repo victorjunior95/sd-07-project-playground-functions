59e428b70784fe971d0132cadb8fdfd6fd80e2b2
richardfazeredo <richardcompletetour@gmail.com>
2020-09-02 15:44:27 -0300

    Divide uma sentença por palavra, em um array de palavras.

--- src/challenges.js
+++ src/challenges.js
@@ -13,8 +13,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(sentense) {
+  let divideSentense = sentense.split(' ');
+  return divideSentense;
 }
 
 // Desafio 4
