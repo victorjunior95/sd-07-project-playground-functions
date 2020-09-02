e15600705d7f234cc0a147c509cc8f7b3aac9c76
Jesus Palli <jepaur5@gmail.com>
2020-09-02 18:43:52 -0300

    requisito1

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,15 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(a, b) {
   // seu código aqui
-  este é só um teste
+  if (a == true && b == true){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
+
+console.log(compareTrue(true,true))
 
 // Desafio 2
 function calcArea() {
