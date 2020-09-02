a4b34f3e6513e6b08e0092b3f497d32386c57f77
Julio Kauer <juliokauer@gmail.com>
2020-09-02 19:17:46 -0300

    challenge triangle

--- src/challenges.js
+++ src/challenges.js
@@ -237,13 +237,15 @@ function generatePhoneNumber(array) {
   return stringPhone;
 }
 
-console.log(generatePhoneNumber( [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck( lineA,  lineB,  lineC ) {
+  if (lineA < lineB + lineC && lineB < lineA + lineC && lineC < lineA + lineB ) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
 // Desafio 13
 function hydrate() {
   // seu código aqui
