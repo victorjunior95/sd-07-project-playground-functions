a4c6e5e87b27490e452012b12285cc794f03653f
Lucas Andrade <lafdesouza2002@outlook.com>
2020-09-02 16:03:41 -0300

    Fixing function CatAndMouse

--- src/challenges.js
+++ src/challenges.js
@@ -38,11 +38,11 @@ function catAndMouse(cat1, cat2, mouse) {
   if (cat1 === cat2){
     return "os gatos trombam e o rato foge"
   }
-  else if (mouse-cat1 < mouse-cat2){
-    return cat1
+  else if (mouse-cat1 > mouse-cat2){
+    return "cat1"
   }
   else{
-    return cat2
+    return "cat2"
   }
 }
 
