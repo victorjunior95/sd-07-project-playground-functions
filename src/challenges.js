9839b9d857124dc2036125ab52f5de18f62e8426
Emanuelle Brasil <emanuelle.rbl@gmail.com>
2020-09-02 16:56:43 -0300

    Correção desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -55,13 +55,13 @@ function highestCount(arrayNumbers) {
 }
 
 // Desafio 7
-function catAndMouse(mouse, cat1, cat2) {
+function catAndMouse(mouse , cat1, cat2) {
   if(cat1 - mouse < cat2 - mouse){
     return "cat1";
-} else if(cat1 - mouse === cat2 - mouse){
-    return "os gatos trombam e o rato foge";
-} else{
+} else if(cat1 - mouse > cat2 - mouse){
     return "cat2";
+} else{
+    return "os gatos trombam e o rato foge";
 }
 }
 
