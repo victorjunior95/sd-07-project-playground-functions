bcc7f76f0168a32228dd4a73482ebd11ed449539
Carol Andrade <a.carolg1@gmail.com>
2020-09-02 18:22:55 -0300

    requisito8 sub1

--- src/challenges.js
+++ src/challenges.js
@@ -1,28 +1,24 @@
 
 // Desafio 1
 function compareTrue(a, b) {
-  // seu código aqui
   let result = (a === true && b === true) ? true : false;
   return (result)
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  // seu código aqui
   let area = (base * height) / 2;
   return (area);
 }
 
 // Desafio 3
 function splitSentence(string) {
-  // seu código aqui
   let array = string.split(' ');
   return (array);
 }
 
 // Desafio 4
 function concatName(array) {
-  // seu código aqui
   let firstItem = array[0];
   let lastItem = array[array.length - 1];
   let string = (`${lastItem}, ${firstItem}`);
@@ -31,14 +27,12 @@ function concatName(array) {
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  // seu código aqui
   let pontos = (wins *= 3) + (ties *= 1);
   return (pontos);
 }
 
 // Desafio 6
 function highestCount(array) {
-  // seu código aqui
   let maiorNumero = '';
   let numeroRepeat = 0;
   for (let i = 0; i < array.length; i += 1) {
@@ -57,12 +51,10 @@ function highestCount(array) {
   return (numeroRepeat);
 }
 
-
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let situation1 = '';
+let situation1 = '';
 let situation2 = '';
-
 if (cat1 - mouse < 0) {
     situation1 = -1 * (cat1 - mouse);
 }
@@ -89,7 +81,19 @@ return(resultado)
 
 // Desafio 8
 function fizzBuzz() {
-  // seu código aqui
+  let string = [];
+for (let i = 0; i < array.length; i += 1)
+    if (array[i] % 3 != 0) {
+        string.push('bug!');
+    } else if (array[i] % 3 === 0 && array[i] % 5 === 0) {
+        string.push('fizzBuzz');
+    } else if (array[i] % 3 === 0) {
+        string.push('fizz');
+    } else if (array[i] % 5 === 0) {
+        string.push('buzz');
+    }
+    return (string);
+    console.log(string);
 }
 
 // Desafio 9
