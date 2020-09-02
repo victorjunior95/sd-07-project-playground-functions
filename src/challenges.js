28aa7fbb00d522165341edbaeccd7418485b1d83
GabrielLennox <meunomeelennox@gmail.com>
2020-09-02 16:56:37 -0300

    Desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -15,8 +15,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  let array = string.split(" ");
+  return array;
 }
 
 // Desafio 4
