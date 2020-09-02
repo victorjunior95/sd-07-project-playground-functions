a32ecbfb1fa65ac79c6879b7fc34517b974caf32
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-09-02 17:09:38 -0300

    adicionando a propriedade ABS

--- src/challenges.js
+++ src/challenges.js
@@ -75,11 +75,11 @@ function catAndMouse(mouse, cat1, cat2) {
   let distanceCat1 = cat1 - mouse;
   let distanceCat2 = cat2 - mouse;
 
-  if (distanceCat1 < distanceCat2) {
-    return cat1;
+  if (distanceCat1.ABS() < distanceCat2.ABS()) {
+    return "cat1";
   }
-  else if (distanceCat2 < distanceCat1){
-    return cat2;
+  else if (distanceCat2.ABS() < distanceCat1.ABS()){
+    return "cat2";
   }
   else {
     return "os gatos trombam e o rato foge";
