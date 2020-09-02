df944ba2b48f50df95724c4e3cfc09e810326ef2
Diego F Affonso <diegofernandesaffonso@gmail.com>
2020-09-02 17:05:00 -0300

    Desafio 2

--- src/challenges.js
+++ src/challenges.js
@@ -6,11 +6,11 @@ function compareTrue(valor1, valor2) {
     return `false`
   }
   }
-}
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2;
+
 }
 
 // Desafio 3
