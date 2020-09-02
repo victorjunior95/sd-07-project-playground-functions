fb73ede3f6912df22d56d74320d7793eb078b3ee
Thays Costa <thayscosta3@gmail.com>
2020-09-02 15:59:05 -0300

    Requisito obrigatório 5

--- src/challenges.js
+++ src/challenges.js
@@ -26,10 +26,12 @@ function concatName(array) {
   return array[array.length - 1] + ", " + array[0];
 }
 console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
+
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + ties;
 }
+console.log(footballPoints(2,3));
 
 // Desafio 6
 function highestCount() {
