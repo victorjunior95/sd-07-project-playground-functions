f26c9d94ee3bed12672b19942447a87a1bcc3661
Rodrigo <rd64103@gmail.com>
2020-09-02 16:58:47 -0300

    Desafio 1 com correçao do climate

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,7 @@
 // Desafio 1
-function compareTrue(a,b) {
-  if(a===true&&b===true) return true
-  else  return false;
+function compareTrue(a, b) {
+  if ( a===true && b===true ) return true
+  return false;
 }
 
 // Desafio 2
