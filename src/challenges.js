80012e0dfdcf522ba2bb85b9baed1a7b0ee34368
Raphael Rodrigues <rapha.rodrigues04@gmail.com>
2020-09-02 18:50:53 -0300

    Testando a resolução do quinto exercicio

--- src/challenges.js
+++ src/challenges.js
@@ -30,8 +30,11 @@ function concatName(wordConcat) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+let wins = 10;
+let ties = 3;
+
+function footballPoints(wins, ties) {
+  return (wins * 3) + (ties * 1);
 }
 
 // Desafio 6
