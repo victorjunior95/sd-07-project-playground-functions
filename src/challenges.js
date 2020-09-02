284d6ec51edaad2373a9632fa555446a54cd4322
mayconfelipes <mayconfelipe.dev@gmail.com>
2020-09-02 17:56:42 -0300

    Requisito 1. VAMOS COM TUDO :rocket:

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-}
+function compareTrue(valor1,valor2) {
+  if (valor1 == true && valor2 == true){
+    return true;
+  }else return false;
+
+compareTrue(true,true);
 
 // Desafio 2
 function calcArea() {
