03d032eb2dc00e016ae814de59c5aa0fcce4f712
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-09-02 19:09:25 -0300

    implementando de formatação

--- src/challenges.js
+++ src/challenges.js
@@ -14,6 +14,7 @@ function calcArea(base, height) {
 function splitSentence(textString) {
   // retorna array de string separada pelos espaços
   let palavra = textString.split(' ');
+
   return palavra;
 }
 
@@ -36,14 +37,14 @@ function highestCount(numbers) {
   let greaterValue = 0;
   let greaterValueCounts = 0;
 
-  for(let index in myArrayValues){
-    if( myArrayValues[index] > greaterValue) {
+  for (let index in myArrayValues) {
+    if (myArrayValues[index] > greaterValue) {
       greaterValue = myArrayValues[index];
-    } 
+    }
   }
 
-  for(let i of myArrayValues){
-    if(greaterValue === i) greaterValueCounts += 1;
+  for (let i of myArrayValues) {
+    if (greaterValue === i) greaterValueCounts += 1;
   }
 
   return greaterValueCounts;
@@ -52,11 +53,11 @@ function highestCount(numbers) {
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
-  if(Math.abs(cat1 - mouse) === Math.abs(cat2 - mouse)){
+  if (Math.abs(cat1 - mouse) === Math.abs(cat2 - mouse)) {
     return "os gatos trombam e o rato foge";
-  }else if(Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)){
+  } else if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
     return "cat2";
-  }else if(Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)){
+  } else if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
     return "cat1";
   }
 }
@@ -67,14 +68,14 @@ function fizzBuzz(numbersArray) {
   const numbers = numbersArray;
   let fizzBussReturned = [];
 
-  for(let num of numbers){
-    if(num % 3 === 0 && num % 5 !== 0){
+  for (let num of numbers) {
+    if (num % 3 === 0 && num % 5 !== 0) {
       fizzBussReturned.push("fizz");
-    }else if(num % 3 !== 0 && num % 5 === 0){
+    } else if (num % 3 !== 0 && num % 5 === 0) {
       fizzBussReturned.push("buzz");
-    }else if(num % 3 === 0 && num % 5 === 0){
+    } else if (num % 3 === 0 && num % 5 === 0) {
       fizzBussReturned.push("fizzBuzz");
-    }else{
+    } else {
       fizzBussReturned.push("bug!");
     }
   }
@@ -89,14 +90,14 @@ function encode(textEncode) {
   // o -> 4 
   // u -> 5
   let encoding = '';
-  for(let i in textEncode){
+  for (let i in textEncode) {
     switch (textEncode[i]) {
       case 'a':
         encoding += 1;
         break;
       case 'e':
         encoding += 2;
-        break; 
+        break;
       case 'i':
         encoding += 3;
         break;
@@ -121,14 +122,14 @@ function decode(textDecode) {
   // 4 -> o 
   // 5 -> u
   let decoding = '';
-  for(let i in textDecode){
+  for (let i in textDecode) {
     switch (textDecode[i]) {
       case '1':
         decoding += 'a';
         break;
       case '2':
         decoding += 'e';
-        break; 
+        break;
       case '3':
         decoding += 'i';
         break;
@@ -149,13 +150,13 @@ function decode(textDecode) {
 // Desafio 10
 function techList(techNames, name) {
   // seu código aqui
-  
+
   const lists = techNames.sort();
 
-  if(lists.length === 0 ) return "Vazio!";
+  if (lists.length === 0) return "Vazio!";
 
   const listReturned = [];
-  for(let list of lists){
+  for (let list of lists) {
     listReturned.push({
       tech: list,
       name: name
@@ -169,19 +170,23 @@ function generatePhoneNumber(phoneNumber) {
   // return phone number formated
   const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  if(phoneNumber.length !== 11) return "Array com tamanho incorreto.";
-  for(let i of phoneNumber){
-    if(!(i in numbers)) return "não é possível gerar um número de telefone com esses valores";
+  //verifica se existe mais ou menos de 11 número e retorna erro
+  if (phoneNumber.length !== 11) return "Array com tamanho incorreto.";
+
+  //verifica se números são diferentes do numbers
+  for (let i of phoneNumber) {
+    if (!(i in numbers)) return "não é possível gerar um número de telefone com esses valores";
   }
+
   let phoneFormated = '(';
-  for(let pos in phoneNumber){
-    if(pos >= 0 && pos <= 1){
+  for (let pos in phoneNumber) {
+    if (pos >= 0 && pos <= 1) {
       phoneFormated += phoneNumber[pos];
-      if(pos == 1) phoneFormated += ') ';
-    }else if(pos >= 2 && pos <= 6){
+      if (pos == 1) phoneFormated += ') ';
+    } else if (pos >= 2 && pos <= 6) {
       phoneFormated += phoneNumber[pos];
-      if(pos == 6) phoneFormated += '-';
-    }else if(pos >= 7 && pos <= 10){
+      if (pos == 6) phoneFormated += '-';
+    } else if (pos >= 7 && pos <= 10) {
       phoneFormated += phoneNumber[pos];
     }
   }
@@ -189,10 +194,10 @@ function generatePhoneNumber(phoneNumber) {
   const myArrayValues = phoneNumber;
   let countRepeatValue = 0;
 
-  for(let i of myArrayValues){
-    for(let j of myArrayValues){
-      if(i === j) countRepeatValue += 1;
-      if(countRepeatValue >= 3) return "não é possível gerar um número de telefone com esses valores";
+  for (let i of myArrayValues) {
+    for (let j of myArrayValues) {
+      if (i === j) countRepeatValue += 1;
+      if (countRepeatValue >= 3) return "não é possível gerar um número de telefone com esses valores";
     }
     countRepeatValue = 0;
   }
@@ -205,9 +210,9 @@ function generatePhoneNumber(phoneNumber) {
 function triangleCheck(lineA, lineB, lineC) {
   // checa se é um trinagulo
   let isTriangle = true;
-  if(!(lineA <= lineB + lineC && lineA > Math.abs(lineB - lineC))) isTriangle = false;
-  if(!(lineB <= lineA + lineC && lineB > Math.abs(lineA - lineC))) isTriangle = false;
-  if(!(lineC <= lineA + lineB && lineC > Math.abs(lineA - lineB))) isTriangle = false;
+  if (!(lineA <= lineB + lineC && lineA > Math.abs(lineB - lineC))) isTriangle = false;
+  if (!(lineB <= lineA + lineC && lineB > Math.abs(lineA - lineC))) isTriangle = false;
+  if (!(lineC <= lineA + lineB && lineC > Math.abs(lineA - lineB))) isTriangle = false;
   return isTriangle;
 }
 
@@ -215,8 +220,8 @@ function triangleCheck(lineA, lineB, lineC) {
 function hydrate(textString) {
   // seu código aqui
   let glassWater = 0;
-  for(let num in textString){
-    if(textString[num] == Number(textString[num])) glassWater += Number(textString[num])
+  for (let num in textString) {
+    if (textString[num] == Number(textString[num])) glassWater += Number(textString[num])
   }
 
   return `${glassWater} copo${glassWater === 1 ? '' : 's'} de água`;
