35d846f9d15ca6a71c7d57ae8ddfb8ed2f087779
Felipe Nascimento <flpnascto@gmail.com>
2020-09-02 20:12:41 -0300

    Corrigindo Desafio 10.

--- src/challenges.js
+++ src/challenges.js
@@ -7,13 +7,13 @@ function compareTrue(valorA, valorB) {
 // Desafio 2
 
 function calcArea(base, height) {
-  return base * height / 2;
+  return (base * height) / 2;
 }
 
 // Desafio 3
 
 function splitSentence(frase) {
-  return frase.split(" ")
+  return frase.split(" ");
 }
 
 // Desafio 4
@@ -27,13 +27,13 @@ function concatName(array) {
 // Desafio 5
 function footballPoints(wins, ties) {
   let total = 0;
-  total = (wins * 3) + ties;
-  return total
+  total = wins * 3 + ties;
+  return total;
 }
 
 // Desafio 6
 
-function maiorValor(numeros){
+function verificaMaiorValor(numeros) {
   let maiorValor = 0;
   for (let i in numeros) {
     if (numeros[i] > maiorValor) {
@@ -45,7 +45,7 @@ function maiorValor(numeros){
 
 function highestCount(numeros) {
   // verifica maior valor
-  let maiorValor = maiorValor(numeros);
+  let maiorValor = verificaMaiorValor(numeros);
 
   // verifica repetição
   let repeticao = 0;
@@ -54,8 +54,9 @@ function highestCount(numeros) {
       repeticao += 1;
     }
   }
-  return repeticao
+  return repeticao;
 }
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
@@ -79,11 +80,11 @@ function catAndMouse(mouse, cat1, cat2) {
 function fizzBuzz(numeros) {
   let msg = [];
   for (let i in numeros) {
-    if (numeros[i] % 3 === 0 && numeros[i] % 5 === 0){
+    if (numeros[i] % 3 === 0 && numeros[i] % 5 === 0) {
       msg.push("fizzBuzz");
-    } else if (numeros[i] % 3 === 0){
+    } else if (numeros[i] % 3 === 0) {
       msg.push("fizz");
-    } else if (numeros[i] % 5 === 0){
+    } else if (numeros[i] % 5 === 0) {
       msg.push("buzz");
     } else {
       msg.push("bug!");
@@ -97,28 +98,52 @@ function encode(frase) {
   let msg = [];
   for (let i in frase) {
     switch (frase[i]) {
-      case "a": msg.push("1"); break;
-      case "e": msg.push("2"); break;
-      case "i": msg.push("3"); break;
-      case "o": msg.push("4"); break;
-      case "u": msg.push("5"); break;
-      default: msg.push(frase[i]);
+      case "a":
+        msg.push("1");
+        break;
+      case "e":
+        msg.push("2");
+        break;
+      case "i":
+        msg.push("3");
+        break;
+      case "o":
+        msg.push("4");
+        break;
+      case "u":
+        msg.push("5");
+        break;
+      default:
+        msg.push(frase[i]);
     }
-  } return msg.join("");
+  }
+  return msg.join("");
 }
 
 function decode(frase) {
   let msg = [];
   for (let i in frase) {
     switch (frase[i]) {
-      case "1": msg.push("a"); break;
-      case "2": msg.push("e"); break;
-      case "3": msg.push("i"); break;
-      case "4": msg.push("o"); break;
-      case "5": msg.push("u"); break;
-      default: msg.push(frase[i]);
+      case "1":
+        msg.push("a");
+        break;
+      case "2":
+        msg.push("e");
+        break;
+      case "3":
+        msg.push("i");
+        break;
+      case "4":
+        msg.push("o");
+        break;
+      case "5":
+        msg.push("u");
+        break;
+      default:
+        msg.push(frase[i]);
     }
-  } return msg.join("");
+  }
+  return msg.join("");
 }
 
 // Desafio 10
@@ -127,7 +152,7 @@ function techList(techName, name) {
     return "Vazio!";
   } else {
     let objectList = [];
-    for(let i in techName) {
+    for (let i in techName.sort()) {
       let obj = {
         tech: "",
         name: "",
@@ -136,43 +161,43 @@ function techList(techName, name) {
       obj.name = name;
       objectList.push(obj);
     }
-    return objectList.sort();
+    return objectList;
   }
-}console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],"Lucas"))
+}
 
 // Desafio 11
 function generatePhoneNumber(phoneNumber) {
   // verifica se o número possui 11 algarismos
-  if (phoneNumber.length != 11){
+  if (phoneNumber.length != 11) {
     return "Array com tamanho incorreto.";
   }
 
   // verifica algarismo menor que zero ou maior que 9
   for (let i in phoneNumber) {
-    if (phoneNumber[i] < 0 || phoneNumber[i] > 9){
+    if (phoneNumber[i] < 0 || phoneNumber[i] > 9) {
       return "não é possível gerar um número de telefone com esses valores";
     }
   }
 
   // verifica se algum algarismo repete mais de 3 vezes
-  for (let i in phoneNumber){
+  for (let i in phoneNumber) {
     let algarismo = phoneNumber[i];
     let repeticao = 0;
-    for (let i in phoneNumber){
+    for (let i in phoneNumber) {
       if (algarismo == phoneNumber[i]) {
         repeticao += 1;
       }
     }
-    if (repeticao >= 3){
+    if (repeticao >= 3) {
       return "não é possível gerar um número de telefone com esses valores";
     }
   }
 
   let formatedPhoneNumber = "(";
-  for (let i in phoneNumber){
-    if (i == 1) {
+  for (let i in phoneNumber) {
+    if (i === "1") {
       formatedPhoneNumber += `${phoneNumber[i]}) `;
-    } else if (i == 6) {
+    } else if (i === "6") {
       formatedPhoneNumber += `${phoneNumber[i]}-`;
     } else {
       formatedPhoneNumber += `${phoneNumber[i]}`;
@@ -183,20 +208,21 @@ function generatePhoneNumber(phoneNumber) {
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-  if ( (lineA <= lineB + lineC) &&
-       (lineB <= lineA + lineC) &&
-       (lineC <= lineB + lineA)) {
-         return true;
-       } else {
-         return false;
-       }
+  if (
+    lineA <= lineB + lineC &&
+    lineB <= lineA + lineC &&
+    lineC <= lineB + lineA
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 13
 function hydrate() {
   // seu código aqui
 }
-
 
 module.exports = {
   calcArea,
@@ -213,4 +239,4 @@ module.exports = {
   hydrate,
   splitSentence,
   triangleCheck,
-}
+};
