6e0011ddf961232e159070dcfaf798cd22d95998
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-09-02 15:29:52 -0300

    iniciando o playground

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,13 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-}
+function compareTrue(valor1,valor2) {
+  if (valor1 === true && valor2 === true) {
+    console.log(true);}
+  else {
+    console.log(false);
+  }
+}  
+
+compareTrue(true,true);
 
 // Desafio 2
 function calcArea() {
