d730a0dc7530c0f85eeaca1783d995e58d85e49f
Fernando Antunes <fernando.antunes1@gmail.com>
2020-09-02 15:08:30 -0300

    Iniciando o projeto. Requisito 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,13 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(a,b) {
+  if (a === true && b === true){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
+//console.log(compareTrue(false,true));
 
 // Desafio 2
 function calcArea() {
