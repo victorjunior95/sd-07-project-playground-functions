751c1098f917612a91cebf5b34d149d67a51c706
alvaro <alvarovascon2@gmail.com>
2020-09-02 15:16:43 -0300

    desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -55,14 +55,27 @@ function highestCount(numbers) {
       highestCount += 1;
     }
   }
-  return highestCount
-}
-console.log(highestCount([9, 1, 9, 3, 9, 5, 7]))
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
+  return highestCount;
 }
 
+// Desafio 7
+function catAndMouse(mouse, cat1, cat2) {
+
+  let distCat1 = Math.abs(cat1 - mouse);
+  let distCat2 = Math.abs(cat2 - mouse);
+  let result ;
+  if (distCat1 === distCat2) {
+    result = "os gatos trombam e o rato foge";
+  } else {
+    if (distCat1 < distCat2) {
+      result = cat1;
+    } else {
+      result = cat2;
+    }
+  }
+  return result
+}
+console.log(catAndMouse(1,3,5))
 // Desafio 8
 function fizzBuzz() {
   // seu código aqui
