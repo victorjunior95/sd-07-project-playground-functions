4825a47d27dcc2abcedb1fdd70a396eff296a9bd
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2020-09-02 16:10:21 -0300

    exercicio 4

--- src/challenges.js
+++ src/challenges.js
@@ -20,8 +20,9 @@ function splitSentence(string) {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let string = `${array[array.length-1]}, ${array[0]}`
+  return string;
 }
 
 // Desafio 5
