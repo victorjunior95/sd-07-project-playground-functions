c1e1896321b2ca16e19e5e9f8f74af18b424df95
hvenses <hvenses@gmail.com>
2020-09-02 16:20:15 -0300

    Comitando exercicio 3.

--- src/challenges.js
+++ src/challenges.js
@@ -14,8 +14,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(sentense) {
+  let splitted = sentense.split(" ");
+  return splitted;
 }
 
 // Desafio 4
