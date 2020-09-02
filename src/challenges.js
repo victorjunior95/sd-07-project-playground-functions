d03b64bb718bcd66d53a68c62c3e7eaf1938496b
samantabelow <samanta.below@gmail.com>
2020-09-02 18:41:14 -0300

    Desafio 12

--- src/challenges.js
+++ src/challenges.js
@@ -197,14 +197,25 @@ function generatePhoneNumber(phoneNumberArray) {
 console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA + lineB > lineC && lineA + lineC > lineB && lineC + lineB > lineA && lineA > Math.abs(lineB - lineC) && lineB > Math.abs(lineA - lineC) && lineC > Math.abs(lineB - lineA))
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
+
+console.log(triangleCheck(10, 14, 8));
 
 // Desafio 13
 function hydrate() {
   // seu código aqui
 }
+
+
 
 
 module.exports = {
