c2f3ba0332ac6ed06c6c5df9087e3e20c9873f52
alexandrefaustino <alexandrefauldade@gmail.com>
2020-09-02 17:01:25 -0300

    Desafio 05 - função footballPoints

--- src/challenges.js
+++ src/challenges.js
@@ -27,8 +27,9 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = (wins * 3) + (ties * 1);
+  return points;
 }
 
 // Desafio 6
