b99ddcb9c290467544d8f506cf3807356a0301af
Vladimir <vladimirrchagas@gmail.com>
2020-09-02 15:07:30 -0300

    Resolução do Desafio 2

--- src/challenges.js
+++ src/challenges.js
@@ -5,8 +5,9 @@ function compareTrue(bolean1, bolean2) {
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  return (base*height/2);
 }
 
 // Desafio 3
