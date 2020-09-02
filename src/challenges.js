f3c8bf3ae2f8347c5ff7f7e907b8544c5e242b12
menezesandre <dehhmenezes87@gmail.com>
2020-09-02 15:41:24 -0300

    Challenge #4

--- src/challenges.js
+++ src/challenges.js
@@ -20,8 +20,9 @@ function splitSentence(string) {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let name = array[array.length-1] + `, ` + array[0];
+  return name;
 }
 
 // Desafio 5
