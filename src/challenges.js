4296e8a8dff6bbca7a0536034f1288b43ac09334
Fernando <nando.neves@gmail.com>
2020-09-02 15:12:38 -0300

    corrigi o cat

--- src/challenges.js
+++ src/challenges.js
@@ -67,9 +67,9 @@ function catAndMouse(mouse, cat1, cat2) {
   let dist1 = Math.abs(cat1 - mouse);
   let dist2 = Math.abs(cat2 - mouse);
   if (dist1 < dist2) {
-    return (cat1);
+    return ("cat1");
   } else if (dist2 < dist1) {
-    return (cat2);
+    return ("cat2");
   } else {
     return "os gatos trombam e o rato foge";
   }
