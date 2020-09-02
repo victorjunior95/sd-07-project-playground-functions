9677cf6723975258a8e93e9932bedb8f99ef9a9e
Bruna Sepulveda <sepulvedabruna@gmail.com>
2020-09-02 15:34:58 -0300

    desafio 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,16 +1,15 @@
 // Desafio 1
 function compareTrue(a,b) {
-  // seu código aqui
   if(a === true && b === true){
     return true
   } 
   else {
     return false};
-
 // Desafio 2
-function calcArea() {
-  // seu código aqui
-}
+function calcArea(base,height) {
+  let area = [(base*height)/2]
+return area
+};
 
 // Desafio 3
 function splitSentence() {
