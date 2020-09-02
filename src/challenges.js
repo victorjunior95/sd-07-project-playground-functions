d951baba468894015576eb3a874b817574181597
Nicholas Torres J. Vasconcelos <nicolastorresjatoba@gmail.com>
2020-09-02 16:54:50 -0300

    Desafios 1, 2 e 3 respondidas

--- src/challenges.js
+++ src/challenges.js
@@ -1,17 +1,29 @@
 // Desafio 1
-function compareTrue() {
-  let teste;
+function compareTrue(bool1, bool2) {
+  return bool1 && bool2;
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height)/2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+  let arrayFrase = [""];
+  let x = 0;
+  for(let i = 0; i < frase.length; i++){
+    if (frase[i] != " "){
+      arrayFrase[x] += frase[i];
+    }else {
+      x += 1;
+      arrayFrase[x] = "";
+    }
+  }
+  return arrayFrase;
 }
+
+console.log(splitSentence("VAI TRYBE pelo amor de Deus"));
 
 // Desafio 4
 function concatName() {
