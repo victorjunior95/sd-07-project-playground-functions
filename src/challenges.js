7b1104cf3c08b9723c14b5c26beb6157d0537c60
lisvalver <lisvalver.atte@gmail.com>
2020-09-02 17:35:57 -0300

    desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -26,11 +26,13 @@ function concatName(array) {
   array = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
   let resultado = (`${array[array.length - 1]}, ${array[0]}`)
   return resultado;
-}
+} console.log(concatName)
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  wins = 3, ties = 1;
+  let pontos = (wins * 3) + (ties);
+  return pontos;
 }
 
 // Desafio 6
