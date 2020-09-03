b177e4f4e3f8ed893421171d898647b02cde86d2
ericvini <ericcobain3@gmail.com>
2020-09-02 21:20:47 -0300

    Ajustes desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -85,11 +85,11 @@ function fizzBuzz(vetor) {
   let vetorSaida = [];
 
   for (let i in array) {
-    if (array[i] % 5 == 0 && array[i] % 3 == 0) {
+    if (((array[i] % 5) == 0 )&& ((array[i] % 3) == 0)) {
       vetorSaida.push("fizzBuz");
-    } else if (array[i] % 5 == 0) {
+    } else if ((array[i] % 5) == 0) {
       vetorSaida.push("buzz");
-    } else if (array[i] % 3 == 0) {
+    } else if ((array[i] % 3) == 0) {
       vetorSaida.push("fizz");
     } else {
       vetorSaida.push("bug!");
