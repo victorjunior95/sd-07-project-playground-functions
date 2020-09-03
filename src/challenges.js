e5a70355bbf34f1c4d6f65fd68efb81db92a7dad
marcelosoliveira <msbobsk8@gmail.com>
2020-09-03 13:59:32 -0300

    desafio 11 numero de telefone

--- src/challenges.js
+++ src/challenges.js
@@ -215,7 +215,7 @@ function generatePhoneNumber(phone) {
   let numberPhone = `(${parte1.join('')})${parte2.join('')}-${parte3.join('')}`;
   return numberPhone;
 }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
+
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
