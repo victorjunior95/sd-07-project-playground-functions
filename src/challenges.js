a5de2147e611d6483d5dd792c659b8f14af3dd25
Vitor Rodrigues <vitor_rc@outlook.com>
2020-09-02 22:49:45 -0300

    adição dos exercícios bonus

--- src/challenges.js
+++ src/challenges.js
@@ -177,18 +177,91 @@ function techList(technologies, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(phone) {
   // seu código aqui
+  let uniqueKeys = phone.values()
+  let formatedPhone = []
+  if (phone.length != 11) {
+    return "Array com tamanho incorreto."
+  }
+
+  for (let key of uniqueKeys) {
+    let cont = 0;
+
+    if (key < 0 || key > 9) {
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+
+    for (let chave in phone) {
+      if (phone[chave] === key) {
+        cont += 1;
+
+      }
+    }
+
+    if (cont >= 3) {
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+
+  }
+
+  for (let cont = 0; cont<11; cont += 1) {
+    if (cont== 0) {
+      formatedPhone.push("(");
+      formatedPhone.push(phone[cont]);
+
+    }
+    else if (cont == 1) {
+      formatedPhone.push(phone[cont]);
+      formatedPhone.push(")");
+
+    }
+    else if (cont == 6) {
+      formatedPhone.push(phone[cont]);
+      formatedPhone.push("-");
+
+    }
+    else {
+      formatedPhone.push(phone[cont]);
+      
+    }
+
+  }
+
+  return formatedPhone.join("");
+
 }
 
 // Desafio 12
-function triangleCheck() {
+function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
+  let acosA = Math.acos(((lineA**2)-(lineB**2)-(lineC**2))/(-2*lineB*lineC))
+  let acosB = Math.acos(((lineB**2)-(lineA**2)-(lineC**2))/(-2*lineA*lineC))
+  let acosC = Math.acos(((lineC**2)-(lineB**2)-(lineA**2))/(-2*lineB*lineA))
+
+  let pi = Math.PI
+  let angulos = acosA + acosB + acosC
+
+  if (angulos==pi){
+      return true
+  }
+  else {
+      return false
+  }
 }
 
 // Desafio 13
-function hydrate() {
+function hydrate(string) {
   // seu código aqui
+  let auxString = 0;
+  for (let char of string) {
+    let charToNumTest = Number.isInteger(Number(char));
+    if (charToNumTest) {
+      auxString += Number(char);
+    }
+  }
+  return `${auxString} copo de água`;
+
 }
 
 
