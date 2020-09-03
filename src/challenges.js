67bc5b20d9085f6f35d3b358ca1e2be691308ecd
ericvini <ericcobain3@gmail.com>
2020-09-02 22:40:58 -0300

    Identação de codigo

--- src/challenges.js
+++ src/challenges.js
@@ -110,24 +110,22 @@ function decode() {
 function techList(array, name) {
   let arrayObjetos = [];
 
-    let nome = name;
+  let nome = name;
 
-    if (array.length == 0) {
-      return "Vazio!";
-    } else {
-      let habilidades = array.sort();
-      for (i in habilidades) {
-        let objeto = {
-          tech: habilidades[i],
-          name: nome,
-        }
-        arrayObjetos.push(objeto);
-      }
-
-
+  if (array.length == 0) {
+    return "Vazio!";
+  } else {
+    let habilidades = array.sort();
+    for (i in habilidades) {
+      let objeto = {
+        tech: habilidades[i],
+        name: nome,
+      };
+      arrayObjetos.push(objeto);
     }
+  }
 
-    return arrayObjetos;
+  return arrayObjetos;
 }
 
 // Desafio 11
