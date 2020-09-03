275de66d860247230e5a278ac6bb0ec5a684ccce
Raphael Caputo <faelelf2@gmail.com>
2020-09-02 23:23:13 -0300

    Consertando quebra de funcao

--- src/challenges.js
+++ src/challenges.js
@@ -63,60 +63,60 @@ function catAndMouse(mouse, cat1, cat2) {
   let distanciaCat1 = cat1 - mouse;
   let distanciaCat2 = cat2 - mouse;
   if (distanciaCat1 > distanciaCat2) {
-    return cat1;
+    return 'cat1';
   } else if (distanciaCat1 < distanciaCat2) {
-    return cat2;
+    return 'cat2';
   } else {
     return "os gatos se trombam e o rato foge"
   }
+}
+// Desafio 8
+function fizzBuzz() {
+  // seu código aqui
+}
 
-  // Desafio 8
-  function fizzBuzz() {
-    // seu código aqui
-  }
+// Desafio 9
+function encode() {
+  // seu código aqui
+}
+function decode() {
+  // seu código aqui
+}
 
-  // Desafio 9
-  function encode() {
-    // seu código aqui
-  }
-  function decode() {
-    // seu código aqui
-  }
+// Desafio 10
+function techList() {
+  // seu código aqui
+}
 
-  // Desafio 10
-  function techList() {
-    // seu código aqui
-  }
+// Desafio 11
+function generatePhoneNumber() {
+  // seu código aqui
+}
 
-  // Desafio 11
-  function generatePhoneNumber() {
-    // seu código aqui
-  }
+// Desafio 12
+function triangleCheck() {
+  // seu código aqui
+}
 
-  // Desafio 12
-  function triangleCheck() {
-    // seu código aqui
-  }
-
-  // Desafio 13
-  function hydrate() {
-    // seu código aqui
-  }
+// Desafio 13
+function hydrate() {
+  // seu código aqui
+}
 
 
-  module.exports = {
-    calcArea,
-    catAndMouse,
-    compareTrue,
-    concatName,
-    decode,
-    encode,
-    fizzBuzz,
-    footballPoints,
-    generatePhoneNumber,
-    techList,
-    highestCount,
-    hydrate,
-    splitSentence,
-    triangleCheck,
-  }
+module.exports = {
+  calcArea,
+  catAndMouse,
+  compareTrue,
+  concatName,
+  decode,
+  encode,
+  fizzBuzz,
+  footballPoints,
+  generatePhoneNumber,
+  techList,
+  highestCount,
+  hydrate,
+  splitSentence,
+  triangleCheck,
+}
