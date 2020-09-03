fe26e635b217bd7b5d3b91d2b7c035d8df6e32bf
Adriano Forcellini <adrianoforcellinipedretti@gmail.com>
2020-09-03 01:41:23 -0300

    ex.12 - agora foi.

--- src/challenges.js
+++ src/challenges.js
@@ -132,10 +132,13 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-    // seu código aqui
+function triangleCheck(a, b, c) {
+    let condicion1 = a < b + c && a > Math.abs(b - c);
+    let condicion2 = b < a + c && b > Math.abs(a - c);
+    let condicion3 = c < b + a && b > Math.abs(b - a);
+    if (condicion1 && condicion2 && condicion3 === true)
+        return true;
 }
-
 // Desafio 13
 function hydrate() {
     // seu código aqui
