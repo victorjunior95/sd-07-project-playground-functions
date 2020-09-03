a699fa60fdc51f6cdb24267f896efeac30336f77
richardfazeredo <richardcompletetour@gmail.com>
2020-09-03 00:55:57 -0300

    Recebe um array de números e retorna o número validado (Corrigindo)

--- src/challenges.js
+++ src/challenges.js
@@ -122,13 +122,13 @@ function techList(arrTech, name) {
 // Desafio 11
 function generatePhoneNumber(telephone) {
   if(telephone.length !== 11){
-    return 'Array com tamanho incorreto.';
+    return "Array com tamanho incorreto.";
   }
   
   let countRepeated = 0;
   for(let i = 0; i < telephone.length; i += 1){
     if(telephone[i] < 0 || telephone[i] > 9 || countRepeated >= 2){
-      return `não é possível gerar um número de telefone com esses valores`;
+      return "não é possível gerar um número de telefone com esses valores";
     }
     if(i !== telephone.indexOf(telephone[i])){
       countRepeated += 1;
@@ -139,9 +139,6 @@ function generatePhoneNumber(telephone) {
   .replace(/(\d{2})(\d{5})(\d{4})/,
    "($1) $2-$3");
 }
-
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
-
 
 // Desafio 12
 function triangleCheck() {
