e29619898aa91e12539bd920457111b3af63ed6c
Carol Andrade <a.carolg1@gmail.com>
2020-09-03 13:24:59 -0300

    codeClimate sub10

--- src/challenges.js
+++ src/challenges.js
@@ -84,12 +84,12 @@ function fizzBuzz(array) {
     if (array[i] % 3 !== 0 && array[i] % 5 !== 0) {
       string.push('bug!');
     } else if (array[i] % 3 === 0 && array[i] % 5 === 0) {
-    string.push('fizzBuzz');
-  } else if (array[i] % 3 === 0) {
-  string.push('fizz');
-} else if (array[i] % 5 === 0) {
-      string.push('buzz');
-    }
+      string.push('fizzBuzz');
+    } else if (array[i] % 3 === 0) {
+    string.push('fizz');
+  } else if (array[i] % 5 === 0) {
+  string.push('buzz');
+}
   }
   return (string);
 }
