396d315574a018747105dfde9f10089bca8e2480
Thays Costa <thayscosta3@gmail.com>
2020-09-03 02:36:03 -0300

    Requisito bônus 11 (falta implementar o teste de limite de repetição)

--- src/challenges.js
+++ src/challenges.js
@@ -168,10 +168,23 @@ function techList(tech, name) {
 }
 console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
 
-// Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+// Desafio 11 - falta testar repetidos
+function generatePhoneNumber(numbers) {
+  let phoneNumber = [];
+  if (numbers.length != 11) {
+    phoneNumber = "Array com tamanho incorreto.";
+  } else {
+    for (let i in numbers) {          
+      if (numbers[i] < 0 || numbers[i] > 9) {
+         phoneNumber = "não é possível gerar um número de telefone com esses valores"
+      } else {
+        phoneNumber = "(" + numbers[0] + numbers[1] + ") "  + numbers[2]  + numbers[3] + numbers[4]  + numbers[5]  + numbers[6]  + "-" + numbers[7] + numbers[8] + numbers[9] + numbers[10];
+          }            
+      }
+  }    
+  return phoneNumber
 }
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 
 // Desafio 12
 function triangleCheck() {
