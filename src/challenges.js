533d52750eb50e3c4ed288ba0c69d2093798ab28
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2020-09-02 22:22:44 -0300

    Desafio 10 - (teste 1)

--- src/challenges.js
+++ src/challenges.js
@@ -226,9 +226,30 @@ function decode(string) {
 }
 
 // Desafio 10
-function techList() {
+let tecnologias=["React", "Jest", "HTML", "CSS", "JavaScript"];
+let name="Lucas"
+function techList(tecnologias,name) {
   // seu código aqui
+  let array=[];
+  tecnologias.sort();
+  if (tecnologias.length == 0)
+  {
+    return("Vazio!");
+  }
+  else
+  {
+    for (let key in tecnologias)
+    {
+      let info = {
+        tech: tecnologias[key],
+        name: name
+      };
+      array.push(info);
+    }
+  }
+  return(array);
 }
+console.log(techList(tecnologias,name));
 
 // Desafio 11
 function generatePhoneNumber() {
