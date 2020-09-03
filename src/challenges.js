2efa24e7de076c7e2d8f5d83cccd0015c6745620
Adriano Forcellini <adrianoforcellinipedretti@gmail.com>
2020-09-02 23:50:59 -0300

    ex7 - distancia dos gatoss

--- src/challenges.js
+++ src/challenges.js
@@ -57,9 +57,9 @@ function highestCount(array) {
 function catAndMouse(mouse, cat1, cat2) {
     if (cat1 - mouse < cat2 - mouse)
         return "cat1";
-    else if (cat1 - mouse > cat2 - mouse) {
+    else if (cat1 - mouse > cat2 - mouse)
         return "cat2";
-    }
+
     return "os gatos trombam e o rato foge";
 }
 
