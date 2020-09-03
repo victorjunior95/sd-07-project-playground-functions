5bb6525c5399e9218ef620d3c1b14ce8dac918f7
Demistocles Hiago <demi.treze@gmail.com>
2020-09-03 20:49:55 -0300

    fazendo alterações no desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -50,20 +50,14 @@ function highestCount(numbers) {
   }  
   return repeticoes[maior]
 }
-  
-
-let resultado = highestCount(numbers)
-console.log(resultado)
-
-
 
 // Desafio 7
 
 
 
 function catAndMouse(mouse, cat1, cat2) {
-  const dist1 = mouse - cat1
-  const dist2 = mouse - cat2
+  const dist1 = Math.abs(mouse - cat1) 
+  const dist2 = Math.abs(mouse - cat2) 
   if ( dist1 < dist2 ) {
     return 'cat1'
   } else if ( dist1 > dist2 ) {
