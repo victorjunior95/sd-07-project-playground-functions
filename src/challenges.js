a17c225e95be6837cd8f65c42416bfde6d60b427
Carol Andrade <a.carolg1@gmail.com>
2020-09-04 10:59:44 -0300

    corrige desafio9

--- src/challenges.js
+++ src/challenges.js
@@ -77,17 +77,14 @@ function fizzBuzz(array) {
 
 // Desafio 9
 function encode(string) {
-
   string = string.replace(/a/g, '1');
   string = string.replace(/e/g, '2');
   string = string.replace(/i/g, '3');
   string = string.replace(/o/g, '4');
   string = string.replace(/u/g, '5');
-
   return string;
 }
 function decode(string) {
-
   string = string.replace(/1/g, 'a');
   string = string.replace(/2/g, 'e');
   string = string.replace(/3/g, 'i');
