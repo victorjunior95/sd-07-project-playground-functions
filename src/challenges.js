809c89457f85f3331069d587d321148373d0d98c
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-09-04 12:47:03 -0300

    Desafio 9 concluído.

--- src/challenges.js
+++ src/challenges.js
@@ -12,14 +12,14 @@ function calcArea(height, base) {
 
 // Desafio 3
 function splitSentence(string) {
-  let splittedSentence = string.split(" ");
+  let splittedSentence = string.split(' ');
   return splittedSentence;
 }
 
 // Desafio 4
 function concatName(posicoesStrings) {
   let lastName = posicoesStrings[posicoesStrings.length - 1];
-  let resultadoString = "";
+  let resultadoString = '';
   resultadoString = `${lastName}, ${posicoesStrings[0]}`;
   return resultadoString;
 }
@@ -58,19 +58,20 @@ function catAndMouse(mouse, cat1, cat2) {
   jerry = mouse;
   tom = cat1;
   garfield = cat2;
-  if (jerry === 1 && tom === 0) {
-    tom = 2;
+  let distanceTom = jerry - tom;
+  let distanceGarfield = jerry - garfield;
+  if (distanceTom < 0) {
+    distanceTom *= -1;
   }
-  if (jerry === 1 && garfield === 0) {
-    garfield = 2;
+  if (distanceGarfield < 0) {
+    distanceGarfield *= -1;
   }
-  if (garfield - jerry === tom - jerry) {
-    return "os gatos trombam e o rato foge";
-  } else if (garfield - jerry < tom - jerry) {
-    console.log("Garfield jantou o Jerry :'(");
+
+  if (distanceGarfield === distanceTom) {
+    return 'os gatos trombam e o rato foge';
+  } else if (distanceGarfield < distanceTom) {
     return `cat2`;
   } else {
-    console.log("Tom jantou o Jerry :'(");
     return `cat1`;
   }
 }
@@ -80,24 +81,71 @@ function fizzBuzz(arrayNumbers) {
   let fizzBuzz = [];
   for (let number in arrayNumbers) {
     if (arrayNumbers[number] % 3 === 0 && arrayNumbers[number] % 5 === 0) {
-      fizzBuzz.push("fizzBuzz");
+      fizzBuzz.push('fizzBuzz');
     } else if (arrayNumbers[number] % 3 === 0) {
-      fizzBuzz.push("fizz");
+      fizzBuzz.push('fizz');
     } else if (arrayNumbers[number] % 5 === 0) {
-      fizzBuzz.push("buzz");
+      fizzBuzz.push('buzz');
     } else {
-      fizzBuzz.push("bug!");
+      fizzBuzz.push('bug!');
     }
   }
   return fizzBuzz;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let word = '';
+  word = string;
+  codedWord = '';
+  for (let letter in word) {
+    switch (word[letter]) {
+      case 'a':
+        codedWord += '1';
+        break;
+      case 'e':
+        codedWord += '2';
+        break;
+      case 'i':
+        codedWord += '3';
+        break;
+      case 'o':
+        codedWord += '4';
+        break;
+      case 'u':
+        codedWord += '5';
+      default:
+        codedWord += word[letter];
+    }
+  }
+  return codedWord;
 }
-function decode() {
-  // seu código aqui
+
+function decode(string) {
+  let word = '';
+  word = string;
+  decodedWord = '';
+  for (let letter in word) {
+    switch (word[letter]) {
+      case '1':
+        decodedWord += 'a';
+        break;
+      case '2':
+        decodedWord += 'e';
+        break;
+      case '3':
+        decodedWord += 'i';
+        break;
+      case '4':
+        decodedWord += 'o';
+        break;
+      case '5':
+        decodedWord += 'u';
+      default:
+        decodedWord += word[letter];
+    }
+  }
+  return decodedWord;
 }
 
 // Desafio 10
