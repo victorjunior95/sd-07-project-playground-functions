ef2b788a39e9d92e7ab36f7c85c3506a41749897
LAURO DE SA <itrafor@gmail.com>
2020-09-04 09:29:05 -0300

    Desafio 2 - Concluido

--- src/challenges.js
+++ src/challenges.js
@@ -12,9 +12,13 @@ function compareTrue(x,y) {
 //console.log("Resultado desafio 1: "+result);
 
 // Desafio 2
-function calcArea() {
+function calcArea(base,height) {
   // seu código aqui
+  let area = (base*height)/2;
+  return area;
 }
+let result = calcArea(10,20);
+console.log("Resultado desafio 2: "+result);
 
 // Desafio 3
 function splitSentence() {
