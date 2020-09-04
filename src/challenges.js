7a232187f6e3f3fc9e57ab451b63ca6d261e8851
vanderson-henrique <vhbenedito@gmail.com>
2020-09-04 19:39:42 -0300

    Alterações de comentários e melhoria no código

--- src/challenges.js
+++ src/challenges.js
@@ -1,55 +1,55 @@
 // Desafio 1 -
 function compareTrue(value1, value2) {
-  if (value1 && value2) {
-    return true;
-  } else {
-    return false;
-  }
+  let estate = false;
+  if (value1 && value2) { // valida se os dois itens são verdadeiros e troca o estado da variável
+    estate = true;
+  } 
+    return estate;
 }
-// console.log(compareTrue(false,false));
+// console.log(compareTrue(true,true));
 
 // Desafio 2
 function calcArea(base, height) {
-  let area = (base * height) / 2;
+  let area = (base * height) / 2; // cálculo básico da área do triângulo.
   return area;
 }
 // console.log(calcArea(10, 4));
 
 // Desafio 3
 function splitSentence(word) {
-  let arrayWord = word.split(' '); // esta função já divide uma string de acordo com o separador que vc define
+  let arrayWord = word.split(' '); // esta função já divide uma string de acordo com o separador que vc define. Neste caso está configurado para dividir a string sempre que encontrar um espaço. Ele também cria um array e coloca cada nova string como novos elementos.
   return arrayWord;
 }
 // console.log(splitSentence("go Trybe"));
 
 // Desafio 4
-function concatName(array) {
-  let primeiroNome = array[0];
-  let ultimoNome = array[array.length - 1];
-  return `${ultimoNome}, ${primeiroNome}`;
+function concatName(array) { // recebe um array como parâmetro
+  let primeiroNome = array[0]; // coloca o primeiro elemento do array na variável
+  let ultimoNome = array[array.length - 1]; // coloca o último elemento (tamanho do array - 1) na variável
+  return `${ultimoNome}, ${primeiroNome}`; // concatena o primeiro e o último nome com uma vírgula
 }
-console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
+// console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
-function footballPoints(wins, ties) {
-  let points = (wins * 3) + ties;
+function footballPoints(wins, ties) { // recebe o número de vitórias e empates como parâmetro
+  let points = (wins * 3) + ties; // cálculo do número de pontos conquistados
   return points;
 }
 // console.log(footballPoints(10, 3));
 
 // Desafio 6
 function highestCount(num) {
-  let maiorValor = 0;
-  let contador = 0;
+  let maiorValor = 0; // guarda o maior valor do array
+  let contador = 0; // guarda o número de vezes que o valor apareceu
 
   for (let i in num) {
     if (num[i] >= maiorValor) {
-      maiorValor = num[i];
+      maiorValor = num[i]; // percorre o array e armazena na variável o maior valor que encontra
     }
   }
   for (let j in num) {
     if (num[j] === maiorValor) {
-      contador += 1;
+      contador += 1; // percorre novamente e sempre que encontrar o maior valor, incremente 1 na variável contador.
     }
   }
   return contador;
@@ -58,36 +58,32 @@ function highestCount(num) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let distCat1 = mouse - cat1;
-  let distCat2 = mouse - cat2;
+  let situation = "";
+  let distCat1 = Math.abs(mouse - cat1); // distância do rato para o gato 1 (valor absoluto)
+  let distCat2 = Math.abs(mouse - cat2); // distância do rato para o gato 2 (valor absoluto)
 
-  if (distCat1 < 0) {
-    distCat1 *= -1;
-  }
-  if (distCat2 < 0) {
-    distCat2 *= -1;
-  }
   if (distCat1 < distCat2) {
-    return 'cat1';
+    situation = 'cat1';
   } else if (distCat2 < distCat1) {
-    return 'cat2';
+    situation = 'cat2';
   } else {
-    return ('os gatos trombam e o rato foge');
+    situation = 'os gatos trombam e o rato foge';
   }
+  return situation;
 }
-// console.log(catAndMouse(10, 4, 22));
+// console.log(catAndMouse(1, 0, 2));
 
 // Desafio 8
 function fizzBuzz(num) {
   let arrayNovo = [];
-  for (let i = 0; i < num.length; i += 1) {
-    if (num[i] % 3 === 0 && num[i] % 5 === 0) {
-      arrayNovo.push('fizzBuzz');
-    } else if (num[i] % 3 === 0) {
+  for (let i = 0; i < num.length; i += 1) { // percorre o array todo
+    if (num[i] % 3 === 0 && num[i] % 5 === 0) { // se for divisível por 3 e por 5
+      arrayNovo.push('fizzBuzz'); // insere esta string
+    } else if (num[i] % 3 === 0) { // se for divisível só por 3
       arrayNovo.push('fizz');
-    } else if (num[i] % 5 === 0) {
+    } else if (num[i] % 5 === 0) { // se for divisível só por 5
       arrayNovo.push('buzz');
-    } else {
+    } else { // se não for divisível nem por 3 nem por 5
       arrayNovo.push('bug!');
     }
   }
