6bca88d48fe3490b6ae73c6ca863f69df4934ee8
Arnaelcio <arnaelcio.gomes@gmail.com>
2020-09-03 22:02:15 -0300

    4o. teste- desafio 1 ao 6

--- src/challenges.js
+++ src/challenges.js
@@ -12,24 +12,41 @@ function calcArea(base, height) {
   return (base*height) /2 
 }
 
-// Desafio 3
-function splitSentence() {
-  // seu código aqui
+// // Desafio 3
+function splitSentence(stringSentence){
+  let resultado = stringSentence.split(" ");
+  return resultado;
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(names) {
+  let amount = names
+  let last = amount[amount.length-1]
+  let first =  amount[0]
+  return last + ", " +  first;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return wins * 3 + ties
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+
+function highestCount(numbers) {
+
+  let inAscendingOrder = numbers.sort()
+  let CounterRepetitions = 0;
+
+  for (i = 0; i< inAscendingOrder.length; i++){
+    if (inAscendingOrder[i]>=inAscendingOrder[i +1]){
+      CounterRepetitions += 1
+    }else if(inAscendingOrder[i +1] === undefined ){
+      return CounterRepetitions
+     }else{
+        CounterRepetitions = 1
+     } 
+  }
 }
 
 // Desafio 7
