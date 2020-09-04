3f113652dfe01023891642b425f7af8bad1a2830
Francisco <franciscoliratorres@gmail.com>
2020-09-04 10:51:59 -0300

    Add mudanças climate code

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
 function compareTrue(bol1, bol2) {
-  if (bol1 == true && bol2 == true){
+  if(bol1 === true && bol2 === true){
     return true;
   } else {
     return false
@@ -12,20 +12,20 @@ function calcArea(base, height) {
   let area = ((base * height) / 2);
   return area
 }
-console.log(calcArea(3,4));
+console.log(calcArea(3, 4));
 
 // Desafio 3
 function splitSentence(string1) {
-  let string2 = string1.split(" ");
+  let string2 = string1.split(' ');
   return string2;
 }
-console.log(splitSentence("Go Trybe"));
+console.log(splitSentence('Go Trybe'));
 
 // Desafio 4
 function concatName(arrayDeStrings) {
-  firstWordlastWord = arrayDeStrings[arrayDeStrings.length - 1] + ", " + arrayDeStrings[0]
+  let firstWordlastWord = arrayDeStrings[arrayDeStrings.length - 1] + ", " + arrayDeStrings[0]
   return firstWordlastWord;
-  }
+}
 console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 // Desafio 5
 function footballPoints(wins, ties) {
@@ -36,18 +36,18 @@ console.log(footballPoints(10, 5))
 // Desafio 6
 function highestCount(arrayDeNumeros) {
   let maiorNumero = 0;
-for (let index in arrayDeNumeros) {
+for (let index in arrayDeNumeros)  {
   if (arrayDeNumeros[index] > maiorNumero) {
     maiorNumero = arrayDeNumeros[index]
   }
 }
-let escolhaNumero = 0;
-for (let j in arrayDeNumeros) {
-  if (arrayDeNumeros[j] == maiorNumero) {
+  let escolhaNumero = 0;
+  for (let j in arrayDeNumeros) {
+  if (arrayDeNumeros[j] === maiorNumero) {
     escolhaNumero += 1
   }
 }
-return escolhaNumero;
+  return escolhaNumero;
 }
 console.log(highestCount([9, 1, 2, 3, 9, 5, 7]))
 
@@ -68,14 +68,14 @@ function catAndMouse(mouse, cat1, cat2) {
 function fizzBuzz(arrayDeNumeros1) {
   let arrayDeNumeros2 = [];
   for (let i = 0; i < arrayDeNumeros1.length; i += 1) {
-    if (arrayDeNumeros1[i] % 3 == 0 && arrayDeNumeros1[i] % 5 !== 0 ) {
-      arrayDeNumeros2.push("fizz");
-    } else if (arrayDeNumeros1[i] % 5 == 0 && arrayDeNumeros1[i] % 3 !== 0) {
-      arrayDeNumeros2.push("buzz")
-    } else if (arrayDeNumeros1[i] % 5 == 0 && arrayDeNumeros1[i] % 3 == 0) {
-      arrayDeNumeros2.push("fizzBuzz")
+    if (arrayDeNumeros1[i] % 3 === 0 && arrayDeNumeros1[i] % 5 !== 0) {
+      arrayDeNumeros2.push('fizz');
+    } else if (arrayDeNumeros1[i] % 5 === 0 && arrayDeNumeros1[i] % 3 !== 0) {
+      arrayDeNumeros2.push('buzz')
+    } else if (arrayDeNumeros1[i] % 5 === 0 && arrayDeNumeros1[i] % 3 === 0) {
+      arrayDeNumeros2.push('fizzBuzz')
     } else {
-      arrayDeNumeros2.push("bug!")
+      arrayDeNumeros2.push('bug!')
     }
   }
   return arrayDeNumeros2
@@ -83,20 +83,20 @@ function fizzBuzz(arrayDeNumeros1) {
 console.log(fizzBuzz([2, 15, 7, 9, 45]));
 // Desafio 9
 function encode(string1) {
-  let mudaVogais = string1.replace(/a/g, "1");
-  let mudaVogai = mudaVogais.replace(/e/g, "2");
-  let mudaVoga = mudaVogai.replace(/i/g, "3");
-  let mudaVog = mudaVoga.replace(/o/g, "4");
-  let mudaVo = mudaVog.replace(/u/g, "5");
+  let mudaVogais = string1.replace(/a/g, '1');
+  let mudaVogai = mudaVogais.replace(/e/g, '2');
+  let mudaVoga = mudaVogai.replace(/i/g, '3');
+  let mudaVog = mudaVoga.replace(/o/g, '4');
+  let mudaVo = mudaVog.replace(/u/g, '5');
   return mudaVo;
 }
 
 function decode(string1) {
-  let mudaVogais = string1.replace(/1/g, "a");
-  let mudaVogai = mudaVogais.replace(/2/g, "e");
-  let mudaVoga = mudaVogai.replace(/3/g, "i");
-  let mudaVog = mudaVoga.replace(/4/g, "o");
-  let mudaVo = mudaVog.replace(/5/g, "u");
+  let mudaVogais = string1.replace(/1/g, 'a');
+  let mudaVogai = mudaVogais.replace(/2/g, 'e');
+  let mudaVoga = mudaVogai.replace(/3/g, 'i');
+  let mudaVog = mudaVoga.replace(/4/g, 'o');
+  let mudaVo = mudaVog.replace(/5/g, 'u');
   return mudaVo;
 }
 
@@ -105,17 +105,17 @@ function techList(tech, name) {
   let array1 = [];
   tech.sort();
   if (tech.length <= 0) {
-    return "Vazio!";
+    return 'Vazio!';
   }
   for (let index in tech) {
     array1.push({
       tech: tech[index],
-      name: name
+      name: name,
     })
   }
   return array1
 }
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
+console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 
 // Desafio 11
 function generatePhoneNumber() {
