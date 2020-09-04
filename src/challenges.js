3c3062e2f449685c9a2d40c647e9a791214571bd
Rodolfo <rodolfojr94@gmail.com>
2020-09-04 10:35:04 -0300

    teste13

--- src/challenges.js
+++ src/challenges.js
@@ -60,15 +60,14 @@ function highestCount(a) {
 
 // Desafio 7
 function catAndMouse(mouse , cat1 , cat2) {
-  if ( (cat1 - mouse) == (cat2 - 0) )
+  if ( Math.abs(mouse - cat1) === Math.abs(mouse - cat2) )
   {
     return "os gatos trombam e o rato foge";
   }
   else
   {
-    return cat1 - mouse > cat2 - mouse ? 'cat2' : 'cat1' ;
+    return Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse) ? 'cat2' : 'cat1' ;
   } ;
-
 }
 
 // Desafio 8
