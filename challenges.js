2348a39d4a03e3d5d262ade8be0f6f4e88fca666
Rogerio-feitor <roger.feitor@gmail.com>
2020-09-05 15:48:04 -0300

    Tentativa de resolução da questão 10.

--- challenges.js
+++ challenges.js
@@ -1,38 +1,42 @@
-// Desafio 4
-// Desafio 9
-function decode(phrase) {
-  let decodigo = phrase.split('');
-  for(let i in decodigo){
-    if (decodigo[i] === '1') {
-      decodigo.splice(i, 1, 'a');
-    } else if (decodigo[i] === '2') {
-      decodigo.splice(i, 1, 'e');
-    } else if (decodigo[i] === '3') {
-      decodigo.splice(i, 1, 'i');
-    } else if (decodigo[i] === '4') {
-      decodigo.splice(i, 1, 'o');
-    } else if (decodigo[i] === '5'){
-      decodigo.splice(i, 1, 'u');
+
+function techList(vetor, name) {
+let result;
+let colecao = [];
+  if(vetor.length < 1 || vetor == null || vetor == undefined) {
+    result = 'Vazio!';
+    return result;
+  } else if(vetor !== null || vetor !== undefined){
+    for(let i in vetor){
+    let lista = {
+      tech:vetor[i],
+      name:name
     }
+    colecao.push(lista);
   }
-return decodigo.join('');
+}
+return colecao.sort(function(a,b){
+  if(a.tech < b.tech) return -1;
+  if(a.tech > b.tech) return 1;
+  return 0;
+})
 }
 
 
 
+
+  console.log(techList(["React", "Jest", "HTML", "CSS", "Rust", "JavaScript"],
+  "Lucas"));
 
 
 
 
 
   
-  // function decode() {
-    // seu código aqui
-  // }
+  
 
     
 
 
-console.log(decode("h1 2h1r2 b3l5 c1f2 t4r4 s1r45 d2m13s"));
+
 
   
--- src/challenges.js
+++ src/challenges.js
@@ -157,9 +157,27 @@ return decodigo.join('');
 }
 
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
+function techList(vetor, name) {
+  let result;
+  let colecao = [];
+    if(vetor.length < 1 || vetor == null || vetor == undefined) {
+      result = 'Vazio!';
+      return result;
+    } else if(vetor !== null || vetor !== undefined){
+      for(let i in vetor){
+      let lista = {
+        tech:vetor[i],
+        name:name
+      }
+      colecao.push(lista);
+    }
+  }
+  return colecao.sort(function(a,b){
+    if(a.tech < b.tech) return -1;
+    if(a.tech > b.tech) return 1;
+    return 0;
+  })
+  }
 
 // Desafio 11
 function generatePhoneNumber() {
