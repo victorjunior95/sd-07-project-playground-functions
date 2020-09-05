cfeae14ef648203ecf2a0b0648fb20ce08eea8ea
josielvs <josielvs@gmail.com>
2020-09-05 18:48:06 -0300

    Code Climate 1

--- src/challenges.js
+++ src/challenges.js
@@ -212,6 +212,11 @@ function generatePhoneNumber(a) {
   if (a.length !== 11) {
     return 'Array com tamanho incorreto.'
   }
+  for (let i = 0; i < a.length; i += 1) {
+    if (a[i] < 0 || a[i] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores'
+    }
+  }
   let countRepeated = 0;
   for (let i = 0; i < a.length; i += 1) {
     let searching = a[i]
@@ -225,7 +230,7 @@ function generatePhoneNumber(a) {
   let phoneComplete = `(${a[0]}${a[1]}) ${a[2]}${a[3]}${a[4]}${a[5]}${a[6]}-${a[7]}${a[8]}${a[9]}${a[10]}`
   return phoneComplete
 }
-console.log(generatePhoneNumber(numbersForPhones))
+generatePhoneNumber(numbersForPhones)
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
@@ -264,7 +269,7 @@ function hydrate(a) {
   strReturn = `${numOfReturn} copo de água`
   return strReturn
 }
-hydrate(strEntry)
+console.log(hydrate(strEntry))
 
 
 module.exports = {
