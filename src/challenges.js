7e5bd3f0a64a604cc1f5022e55c28223b1b718b3
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-09-05 19:30:55 -0300

    Exercício 12 - 05/09

--- src/challenges.js
+++ src/challenges.js
@@ -212,12 +212,21 @@ function generatePhoneNumber(array11) {
   let mensagem = `${phoneNumber1} ${phoneNumber2Join}-${phoneNumber3Join}`;
   return mensagem;
 }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA,lineB,lineC) {
+  let a = Math.abs(lineA);
+  let b = Math.abs(lineB);
+  let c = Math.abs(lineC);
+  
+  if(a < b + c && b < a + c && c < a + b) {
+    return true;
+  } else {
+    return false;
+  }
 }
+console.log(triangleCheck(10,14,8));
 
 // Desafio 13
 function hydrate() {
