ebaa21c4c4f122333f02e825266e03f42d0c1954
Victor Junior <victorjr.muchacho@gmail.com>
2020-09-06 15:20:40 -0400

    Desafio 5 resolvido

--- src/challenges.js
+++ src/challenges.js
@@ -45,7 +45,7 @@ function splitSentence(string) {
 }
 
 // Desafio 4
-/* Escreva uma função com o nome concatNameque, ao receber uma matriz de strings, retorne uma string com o formato 'ÚLTIMO ITEM, PRIMEIRO ITEM', independente do tamanho da matriz.
+/* Escreva uma função com o nome concatName que, ao receber uma matriz de strings, retorne uma string com o formato 'ÚLTIMO ITEM, PRIMEIRO ITEM', independente do tamanho da matriz.
 
 Isso quer dizer que, caso o parâmetro passado para concatName seja a Array ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'], a função deve retornar Paolillo, Lucas. */
 function concatName(array) {
@@ -55,67 +55,70 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+/* Escreva uma função com o nome footballPoints que receba o número de vitórias (esse parâmetro deve se chamar wins) e o número de empates (esse parâmetro deve se chamar ties) e retorne a quantidade de pontos que o time marcou em um campeonato.
+
+Para tanto, considerar que cada vitória vale 3 pontos e cada empate vale 1 ponto. */
+function footballPoints(wins, ties) {
+  return (wins * 3) + ties;
 }
 
-// Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+// // Desafio 6
+// function highestCount() {
+//   // seu código aqui
+// }
 
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+// // Desafio 7
+// function catAndMouse() {
+//   // seu código aqui
+// }
 
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+// // Desafio 8
+// function fizzBuzz() {
+//   // seu código aqui
+// }
 
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
+// // Desafio 9
+// function encode() {
+//   // seu código aqui
+// }
+// function decode() {
+//   // seu código aqui
+// }
 
-// Desafio 10
-function techList() {
-  // seu código aqui
-}
+// // Desafio 10
+// function techList() {
+//   // seu código aqui
+// }
 
-// Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+// // Desafio 11
+// function generatePhoneNumber() {
+//   // seu código aqui
+// }
 
-// Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+// // Desafio 12
+// function triangleCheck() {
+//   // seu código aqui
+// }
 
-// Desafio 13
-function hydrate() {
-  // seu código aqui
-}
+// // Desafio 13
+// function hydrate() {
+//   // seu código aqui
+// }
 
 
-module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  generatePhoneNumber,
-  techList,
-  highestCount,
-  hydrate,
-  splitSentence,
-  triangleCheck,
-}
+// module.exports = {
+//   calcArea,
+//   catAndMouse,
+//   compareTrue,
+//   concatName,
+//   decode,
+//   encode,
+//   fizzBuzz,
+//   footballPoints,
+//   generatePhoneNumber,
+//   techList,
+//   highestCount,
+//   hydrate,
+//   splitSentence,
+//   triangleCheck,
+// }
