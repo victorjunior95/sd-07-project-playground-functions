eb767cbb2aa530de2c6f3d7f71a0bb2b6a1cc370
Victor Junior <victorjr.muchacho@gmail.com>
2020-09-06 11:13:41 -0400

    Desafio 3 resolvido

--- src/challenges.js
+++ src/challenges.js
@@ -34,77 +34,88 @@ function calcArea(base, height) {
 // calcArea(base, height);
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+/* Escreva uma função com o nome splitSentence, um qual receberá uma string e retornará uma matriz de strings separada por cada espaço na string original.
+
+Exemplo: se uma função receber uma string "go Trybe", o retorno deve ser ['go', 'Trybe'].*/
+// let string = "go Trybe"
+
+function splitSentence(string) {
+  let stringSplit = string.split(" ");
+
+  return stringSplit
 }
 
-// Desafio 4
-function concatName() {
-  // seu código aqui
-}
+// function splitSentence(string) {
+//   let strings = []; 
+// }
 
-// Desafio 5
-function footballPoints() {
-  // seu código aqui
-}
+// // Desafio 4
+// function concatName() {
+//   // seu código aqui
+// }
 
-// Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+// // Desafio 5
+// function footballPoints() {
+//   // seu código aqui
+// }
 
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+// // Desafio 6
+// function highestCount() {
+//   // seu código aqui
+// }
 
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+// // Desafio 7
+// function catAndMouse() {
+//   // seu código aqui
+// }
 
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
+// // Desafio 8
+// function fizzBuzz() {
+//   // seu código aqui
+// }
 
-// Desafio 10
-function techList() {
-  // seu código aqui
-}
+// // Desafio 9
+// function encode() {
+//   // seu código aqui
+// }
+// function decode() {
+//   // seu código aqui
+// }
 
-// Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+// // Desafio 10
+// function techList() {
+//   // seu código aqui
+// }
 
-// Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+// // Desafio 11
+// function generatePhoneNumber() {
+//   // seu código aqui
+// }
 
-// Desafio 13
-function hydrate() {
-  // seu código aqui
-}
+// // Desafio 12
+// function triangleCheck() {
+//   // seu código aqui
+// }
+
+// // Desafio 13
+// function hydrate() {
+//   // seu código aqui
+// }
 
 
-module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  generatePhoneNumber,
-  techList,
-  highestCount,
-  hydrate,
-  splitSentence,
-  triangleCheck,
-}
+// module.exports = {
+//   calcArea,
+//   catAndMouse,
+//   compareTrue,
+//   concatName,
+//   decode,
+//   encode,
+//   fizzBuzz,
+//   footballPoints,
+//   generatePhoneNumber,
+//   techList,
+//   highestCount,
+//   hydrate,
+//   splitSentence,
+//   triangleCheck,
+// }
