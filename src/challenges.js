05d896a5d75dbbc2843bf62100c67dac25b17fc7
Arnaelcio <arnaelcio.gomes@gmail.com>
2020-09-06 03:04:44 -0300

    desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -97,11 +97,58 @@ function fizzBuzz(numbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(phrase) {
+  let encryptedLetters = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  }
+
+let encoded = [];
+let result = '';
+
+for ( i = 0; i < phrase.length; i++){
+  encoded[i] = phrase[i];
+for (let index = 0; index< encoded.length; index++){
+    if (encryptedLetters[encoded[i]] == undefined){
+      result += encoded[i]
+      break
+    }else{
+      result += encryptedLetters[encoded[i]]
+      break
+    }
+ } 
 }
-function decode() {
-  // seu código aqui
+ return result
+}
+
+function decode(phrase) {
+   let encryptedLetters = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  }
+
+let decoded = [];
+let result = '';
+
+for ( i = 0; i < phrase.length; i++){
+  decoded[i] = phrase[i];
+for (let index = 0; index< decoded.length; index++){
+    if (encryptedLetters[decoded[i]] == undefined){
+      result += decoded[i]
+      break
+    }else{
+      result += encryptedLetters[decoded[i]]
+      break
+    }
+ } 
+}
+ return result
 }
 
 // Desafio 10
