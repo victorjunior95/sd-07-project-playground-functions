7bd2197f0dcd3df0077ea915a0a5a60aa1398849
murilopereira006 <muriloedu006@gmail.com>
2020-09-06 01:26:48 -0300

    sempre achei q o 7 tava errado

--- src/challenges.js
+++ src/challenges.js
@@ -62,10 +62,10 @@ function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
   var dist1 = cat1 - mouse;
   var dist2 = cat2 - mouse;
-  if (dist1 < dist2) {
+  if (dist1 > dist2) {
     return "cat1"
   }
-  if (dist1 > dist2) {
+  if (dist1 < dist2) {
     return "cat2"
   }
   else {
