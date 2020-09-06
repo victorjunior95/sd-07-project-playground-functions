feb6c887812097b2061e5fa453d984cd136402bc
Victor Junior <victorjr.muchacho@gmail.com>
2020-09-06 18:48:46 -0400

    Desafio 7 resolvido

--- src/challenges.js
+++ src/challenges.js
@@ -82,8 +82,23 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+/* Imagine que existem dois gatos, os quais chamaremos de cat1 e cat2, e que ambos estão atrás de um rato chamado mouse. Imagine que cada um dos três animais está em uma posição representada por um número.
+
+Sabendo disso, crie uma função chamada catAndMouseque, ao receber a posição de mouse, cat1 e cat2, nessa ordem , calcule as distâncias entre o rato e os gatos e retorne qual dos felinos irá alcançar o rato primeiro (sendo aquele que estará mais perto).
+
+Exemplo: caso o gato cat2 está a 2 unidades de distância do rato, e cat1 está a 3 unidades, sua função deve retornar cat2.
+
+Caso os gatos estejam na mesma distância do rato, a função deve retornar uma string "os gatos trombam e o rato foge". */
+function catAndMouse(mouse, cat1, cat2) {
+  let distanceCat1 = mouse - cat1;
+  let distanceCat2 = mouse - cat2;
+  if (distanceCat1 < distanceCat2) {
+    return "cat1"
+  } else if (distanceCat2 < distanceCat1) {
+    return "cat2"
+  } else {
+    return "os gatos trombam e o rato foge"
+  }
 }
 
 // Desafio 8
