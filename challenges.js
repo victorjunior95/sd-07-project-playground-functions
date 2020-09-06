d2426c75c8f59fd362119ed9410e596a61b67a66
Rogerio-feitor <roger.feitor@gmail.com>
2020-09-06 10:09:38 -0300

    Tentativa de resolução da questão 12. Uso de estruturas condicionais e variáveis booleanas.

--- challenges.js
+++ challenges.js
@@ -1,40 +1,29 @@
 
-function generatePhoneNumber(vetor) {
-let countRepeat = vetor.filter((e, i, a) => a.indexOf(e) !== i);
-let contagemReal = 0
-countRepeat.sort(function(a, b) {
-  return a - b;
-});
-for(let i = 0; i < countRepeat.length; i += 1){
-  if(countRepeat[i] === countRepeat[i+1] || countRepeat[i] === countRepeat[i+2]){
-    contagemReal = 3;    
+function triangleCheck(lineA, lineB, lineC) {
+ let condition1;
+ let condition2;
+  if(lineA + lineB > lineC && lineA + lineC > lineB && lineB + lineC > lineA){
+    condition1 =  true;
+  } else {
+        condition1 = false;
   }
-}
-let confere = 0;
-for (let i in vetor) {
-    if (vetor[i] >  9 || vetor[i] < 0){
-        confere++;
-    }
-}
-      if (vetor.length !== 11) {
-      let result = "Array com tamanho incorreto.";
-      return result;
-    }
-    if(contagemReal >= 3 || confere != 0) {
-      let result1 = "não é possível gerar um número de telefone com esses valores";
-      return result1;
-}     vetor.splice(0, 0, '(');
-      vetor.splice(3, 0, ')');
-      vetor.splice(4, 0, ' ');
-      vetor.splice(10, 0, '-');
-      return vetor.join('');
+  if(Math.abs(lineA - lineB) < lineC && Math.abs(lineA - lineC) < lineB && Math.abs(lineB - lineC) < lineA){
+    condition2 = true;
+  } else {
+    condition2 = false
+  }
+  if(condition1 && condition2) {
+    return true;
+  } else {
+    return false;
+  }
 
 }
 
 
 
 
-  console.log(generatePhoneNumber([0, 2, 3, 4, 5, 7, 7, 8, 9, 0, 7]));
+  console.log(triangleCheck(10, 18, 28));
 
 
 
--- src/challenges.js
+++ src/challenges.js
@@ -213,9 +213,25 @@ function generatePhoneNumber(vetor) {
 }  
   
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+function triangleCheck(lineA, lineB, lineC) {
+  let condition1;
+  let condition2;
+   if(lineA + lineB > lineC && lineA + lineC > lineB && lineB + lineC > lineA){
+     condition1 =  true;
+   } else {
+         condition1 = false;
+   }
+   if(Math.abs(lineA - lineB) < lineC && Math.abs(lineA - lineC) < lineB && Math.abs(lineB - lineC) < lineA){
+     condition2 = true;
+   } else {
+     condition2 = false
+   }
+   if(condition1 && condition2) {
+     return true;
+   } else {
+     return false;
+  }
+ }
 
 // Desafio 13
 function hydrate() {
