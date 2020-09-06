cc2119b5775890f730a1a47a05ca5c86f1f4481f
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-09-06 09:14:34 -0400

    função desafio 4 modificada

--- src/challenges.js
+++ src/challenges.js
@@ -21,7 +21,7 @@ function splitSentence(frase) {
 function concatName(frase) {
   let res = [];
   res.push(frase[frase.length - 1]);
-  res.push(frase[0]);
+  res.push(" " + frase[0]);
   return `${res}`;
 }
 
