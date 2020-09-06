f243bf41eea88d050d12df15461823df1e5c2f4e
Vitor Amorims <vtamorims@gmail.com>
2020-09-06 09:03:37 -0300

    code climate function generatePhoneNumber correçao do nome da variavel e espacos.

--- src/challenges.js
+++ src/challenges.js
@@ -173,27 +173,27 @@ function techList(tech, name) {
 }
 // Desafio 11
 function generatePhoneNumber(param) {
-  let numeros = param;
+  let phone = param;
   let retornarphone;
   let cont = 0;
-  if (numeros.length != 11){
-    retornarphone = "Array com tamanho incorreto";
-  }else{
-    for (let i = 0; i < numeros.length; i++){
-      if (numeros[i] < 0 || numeros[i] > 9){
-        retornarphone = "não é possível gerar um número de telefone com esses valores";
-      }else{
-        for (let j = i + 1; j < numeros.length; j++){
-          if (numeros[i] == numeros[j]){
-            cont++;
+  if (phone.length != 11) {
+    retornarphone = 'Array com tamanho incorreto';
+  } else {
+    for (let i = 0; i < phone.length; i += 1) {
+      if (phone[i] < 0 || phone[i] > 9) {
+        retornarphone = 'não é possível gerar um número de telefone com esses valores';
+      } else {
+        for (let j = i + 1; j < phone.length; j += 1) {
+          if (phone[i] == phone[j]) {
+            cont += 1;
           }
         }
       }
     }
-    if (cont >= 3){
-      retornarphone = "não é possível gerar um número de telefone com esses valores";
-    }else{
-      retornarphone = numeros.join('').toString();
+    if (cont >= 3) {
+      retornarphone = 'não é possível gerar um número de telefone com esses valores';
+    } else {
+      retornarphone = phone.join('').toString();
       console.log('('+retornarphone[0]+retornarphone[1]+')'+' '+retornarphone[2]+retornarphone[3]+retornarphone[4]+retornarphone[5]+retornarphone[6]+'-'+retornarphone[7]+retornarphone[8]+retornarphone[9]+retornarphone[10]);
     }
   }
