a0a3d2a5f42b56bb913419a417fd42e459643a40
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-09-07 20:23:57 -0300

    ajustando condição correta requisito 11

--- src/challenges.js
+++ src/challenges.js
@@ -154,8 +154,7 @@ function generatePhoneNumber(number) {
     if (phone[index] < 0 || phone[index] > 9) {
       return "não é possível gerar um número de telefone com esses valores";
     }    
-  }
-  phone = Math.abs(phone);
+  }  
     return `(${phone[0]}${phone[1]}) ${phone[2]}${phone[3]}${phone[4]}${phone[5]}${phone[6]}-${phone[7]}${phone[8]}${phone[9]}${phone[10]}`;
 }
 
