c1780ac5ce6e35a6dcac51071b95abdd220adc81
kramersilva <kramerscs@gmail.com>
2020-09-07 18:04:57 -0300

    code

--- src/challenges.js
+++ src/challenges.js
@@ -189,18 +189,33 @@ function techList(tech, name) {
 }
 
 // Suporte Desafio 11
+function numberGreaterNineLessZero(numbers) {
+  let messageError = 'não é possível gerar um número de telefone com esses valores'
+  let finalMessage = ''
+  let message = ''
+  /* Checking if there is any number less than 0 or greater than nine */
+  for (let number in numbers) {
+    if (numbers[number] > 9 || numbers[number] < 0) {
+      message = 'não é possível gerar um número de telefone com esses valores'
+    }
+  }
+  if (message === messageError) {
+    finalMessage = messageError
+  } else {
+    finalMessage = 'ok'
+  }
+  return finalMessage
+}
+
+// Suporte Desafio 11
 function checkConstraints(numbers) {
   let message = 'No constraints'
+  let messageError = 'não é possível gerar um número de telefone com esses valores'
   /* Checking if the arrays.lenght is greater than 11 */
   if (numbers.length !== 11) {
     message = 'Array com tamanho incorreto.'
-  } else {
-    /* Checking if there is any number less than 0 or greater than nine */
-    for (let number in numbers) {
-      if (numbers[number] > 9 || numbers[number] < 0) {
-        message = 'não é possível gerar um número de telefone com esses valores'
-      }
-    }
+  } else if (numberGreaterNineLessZero(numbers) === messageError) {
+    message = messageError
   }
   return message
 }
@@ -224,7 +239,6 @@ function mostRepeatedNumber(numbers) {
   return mostRepeated
 }
 
-console.log(checkConstraints([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 // Desafio 11
 function generatePhoneNumber(numbers) {
   let constraints = checkConstraints(numbers)
@@ -241,7 +255,7 @@ function generatePhoneNumber(numbers) {
   }
   return message
 }
-
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, -1]))
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   let lineACheck = (lineA < lineB + lineC && lineA > Math.abs(lineB - lineC))
