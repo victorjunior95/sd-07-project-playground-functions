dbcba3641f17a5198ec223353f97895904e896e8
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-09-07 18:40:15 -0300

    code climate

--- src/challenges.js
+++ src/challenges.js
@@ -141,8 +141,7 @@ techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas');
 function generatePhoneNumber(array) {
   if (array.length !== 11) {
     return 'Array com tamanho incorreto.';
-  } else {
-    for (let i = 0; i < array.length; i += 1) {
+  } for (let i = 0; i < array.length; i += 1) {
       let count = 0;
       for (let j = 0; j < array.length; j += 1) {
         if (array[j] === array[i]) {
@@ -152,12 +151,12 @@ function generatePhoneNumber(array) {
       if (array[i] < 0 || array[i] > 9 || count >= 3) {
         return 'não é possível gerar um número de telefone com esses valores';
       }
-    }
     return `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
   }
 }
 
-generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 1]);
+generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]);
+
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
