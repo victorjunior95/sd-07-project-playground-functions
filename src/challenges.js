432a3b98d8326c65aaabdaecac7b66a1f9111972
LAURO DE SA <itrafor@gmail.com>
2020-09-07 08:33:49 -0300

    alteração desafio 8 070901

--- src/challenges.js
+++ src/challenges.js
@@ -120,20 +120,20 @@ function catAndMouse(mouse,cat1,cat2) {
 // Desafio 8
 let numbers = [2, 15, 7, 9, 45];
 let resultado =[];
-function fizzBuzz(n) {
+function fizzBuzz(numbers) {
   // seu código aqui
   for (let i in numbers){
-    if(numbers[i]%3==0 && numbers[i]%5===0){
-      resultado.push("fizzBuzz");
-    }else if(numbers[i]%3==0){
-      resultado.push("fizz");
-    }else if(numbers[i]%5==0){
-      resultado.push("buzz");
+    if((numbers[i] % 3 === 0) && (numbers[i] % 5 === 0)){
+      resultado.push("fizzBuzz");     
+    }else if(numbers[i] % 3 === 0){
+      resultado.push("fizz");     
+    }else if(numbers[i] % 5 === 0){
+      resultado.push("buzz");      
     }else{
-     resultado.push("bug!");
+     resultado.push("bug!");     
     }    
-}
-return resultado;
+  }
+  return resultado;
 }
 //let result = fizzBuzz(numbers);
 //console.log(result);
