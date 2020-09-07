fd3829dd00ddfbc92e58fcddbfd2def578262e3c
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-09-07 18:30:08 -0300

    code climate

--- src/challenges.js
+++ src/challenges.js
@@ -140,12 +140,32 @@ function techList(arrayTechs, name) {
 techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas');
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  if(array.length != 11){
+    console.log('Array com tamanho incorreto.');
+    return 'Array com tamanho incorreto.';
+  } else {
+    for (i = 0; i < array.length; i += 1){
+      let count = 0;
+      for (j = 0; j < array.length; j++) {
+        if(array[j] === array[i]) {
+          count += 1;
+        }
+      }
+      if (array[i] < 0 || array[i] > 9 || count >= 3) {
+        console.log('não é possível gerar um número de telefone com esses valores');
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+    console.log(`(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`)
+  }
 }
 
+generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]);
+
 // Desafio 12
-function triangleCheck() {
+function triangleCheck(lineA, lineB, lineC) {
+  
   // seu código aqui
 }
 
