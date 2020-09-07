1517516f4f6aaee9d7b03d9e12709efdbacba6dd
Carol Andrade <a.carolg1@gmail.com>
2020-09-07 11:13:26 -0300

    codeClimate 2.6

--- src/challenges.js
+++ src/challenges.js
@@ -68,14 +68,16 @@ function catAndMouse(mouse, cat1, cat2) {
 // Desafio 8
 function checkNumber(array) {
   let check = [];
-  if (array[i] % 3 === 0 && array[i] % 5 === 0) {
-    check = 'fizzBuzz';
-  } else if (array[i] % 3 === 0) {
-    check = 'fizz';
-  } else if (array[i] % 5 === 0) {
-    check = 'buzz';
-  } else {
-    check = 'bug!';
+  for (let i in array) {
+    if (array[i] % 3 === 0 && array[i] % 5 === 0) {
+      check = 'fizzBuzz';
+    } else if (array[i] % 3 === 0) {
+      check = 'fizz';
+    } else if (array[i] % 5 === 0) {
+      check = 'buzz';
+    } else {
+      check = 'bug!';
+    }
   }
   return check;
 }
