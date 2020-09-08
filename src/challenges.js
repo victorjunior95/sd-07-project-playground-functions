267cc1b956ad241f8bc95156ad26bab14f5a1adb
Wendrick <wendrick.b.c1@gmail.com>
2020-09-08 18:13:01 -0300

    Desafio 11

--- src/challenges.js
+++ src/challenges.js
@@ -120,9 +120,38 @@ function techList(tech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  
+function generatePhoneNumber(array) {
+  if (array.length === 11) {
+    let listaTelefonica = '(XX) XXXXX-XXXX';
+    let indicesTelefonica = [];
+    let idx = listaTelefonica.indexOf('X');
+    while (idx != -1) {
+      indicesTelefonica.push(idx);
+      idx = listaTelefonica.indexOf('X', idx + 1);
+    }
+    for (let i = 0; i < array.length; i += 1) {
+      let helper = 0;
+      if (array[i] < 0 || array[i] > 9) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+      for (let j = 0; j < array.length; j += 1) {
+        if (array[i] === array[j]){
+          helper += 1;
+          if (helper >= 3){
+            return 'não é possível gerar um número de telefone com esses valores';
+          }
+        }
+      }
+    }
+    for (let i in array) {
+      listaTelefonica = listaTelefonica.replace(listaTelefonica[indicesTelefonica[i]], array[i]);
+    }
+    return listaTelefonica;
+  }
+  return 'Array com tamanho incorreto.';
 }
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
