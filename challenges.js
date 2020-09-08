adfb5e493ba7f836d686e00b1bc352f1ac44e189
Damiem <damiemblackwood@gmail.com>
2020-09-08 18:05:31 -0300

    primeira função 02-1

--- challenges.js
+++ challenges.js
@@ -1,10 +1,18 @@
-
-function compareTrue(parameters1,parameters2){
+//Voltar nesta função mais tarde :c
+function compareTrue(parameters1 , parameters2){
     if(parameters1 && parameters2 == true){
         let resultado = true;
         console.log(resultado)
-    }else{
+    }else {
         let resultado = false;
         console.log(resultado)
     }
+}
+
+//Função funcionando por enquanto :3
+function calcArea(base,Height){
+    let bases= base
+    let altura= Height
+    let soma= base*altura/2
+    console.log(soma)
 }
