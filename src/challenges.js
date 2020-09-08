41e076f6af3100900c76377f37ca2d35abbc5a59
Eduardo Souza <dado.eletrons@gmail.com>
2020-09-08 04:30:36 -0300

    code climate 6

--- src/challenges.js
+++ src/challenges.js
@@ -88,7 +88,7 @@ function highestCount(arr) {
   } */
   // ou podemos fazer um loop mais otimizado
   for (let i = 1; arr[i - 1] === arr[i]; i += 1) {
-    contador++;
+    contador += 1;
   }
   return contador;
 }
