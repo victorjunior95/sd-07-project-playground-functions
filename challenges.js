673c98adf5286f2cd4a5437e3961b327d11815e0
Damiem <damiemblackwood@gmail.com>
2020-09-08 17:45:24 -0300

    primeira função t-01

---
+++ challenges.js
@@ -0,0 +1,11 @@
+
+compareTrue("string","strings")
+function compareTrue(parameters1,parameters2){
+    if(parameters1 && parameters2 == true){
+        let resultado = true;
+        console.log(resultado)
+    }else{
+        let resultado = false;
+        console.log(resultado)
+    }
+}
