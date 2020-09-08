47e6ecd94d5e7dcaeb74c539ec74c58b19caff14
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-09-08 00:07:59 -0300

    corrigindo mais indentação requisito 11

--- src/challenges.js
+++ src/challenges.js
@@ -160,16 +160,16 @@ function checkNumber(number) {
   let repetidos = [];
 
   number.forEach(function (item) {
-    if(!numeros[item]) {
+    if (!numeros[item]) {
       numeros[item] = 0;
       numeros[item] += 1;
     }
   })
 
   for (let prop in numeros) {
-      if(numeros[prop] >= 3) {
-        repetidos.push(prop);
-      }
+    if (numeros[prop] >= 3) {
+      repetidos.push(prop);
+    }
   }
   if (repetidos.length > 0) {
     return 3;
