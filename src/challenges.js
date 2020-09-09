2c9169ddc723e5d95e37c250838ab69f6c41f891
Thays Costa <thayscosta3@gmail.com>
2020-09-09 00:21:10 -0300

    Questão bônus 11 - Agora vai!

--- src/challenges.js
+++ src/challenges.js
@@ -183,14 +183,14 @@ function generatePhoneNumber(array) {
           numberOfTimes += 1;
       }
     }
-    if (numberOfTimes > 2) {
+    if (numberOfTimes => 3) {
       return "não é possível gerar um número de telefone com esses valores";    
     } else {
       return "(" + array[0] + array[1] + ") "  + array[2]  + array[3] + array[4]  + array[5]  + array[6]  + "-" + array[7] + array[8] + array[9] + array[10];
     }
   }    
 }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+console.log(generatePhoneNumber([0, 2, 3, 4, 4, 2, 7, 8, 9, 9, 4]));
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
