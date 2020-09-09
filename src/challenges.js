b295508c9375e098eaa80f0dcba7d16f12caa8b3
vicoliveir <victorhugoqxd@gmail.com>
2020-09-09 14:08:43 -0300

    agora vai

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,9 @@
 // Desafio 1
-    let num1 = 1;
-    let num2 = 2;
 function compareTrue(num1,num2) {
-  if((num1>=0) && (num2>=0)){
+  if(num1 && num2){
     return true;
   }
-  else if((num1>=0) && (num2<0)){
+  else {
     return false; 
   }
   
