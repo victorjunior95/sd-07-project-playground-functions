b819012337e3a7467942ab6c8f9ec8064f298859
vicoliveir <victorhugoqxd@gmail.com>
2020-09-09 14:02:09 -0300

    adicionando parenteses no desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -2,10 +2,10 @@
     let num1 = 1;
     let num2 = 2;
 function compareTrue(num1,num2) {
-  if(num1>=0 && num2>=0){
+  if((num1>=0) && (num2>=0)){
     return true;
   }
-  else if(num1>=0 && num2<0){
+  else if((num1>=0) && (num2<0)){
     return false; 
   }
   
