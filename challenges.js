cc6e623e6484550667d4b2b29cf6f97f5ea8f6f3
Damiem <damiemblackwood@gmail.com>
2020-09-09 02:12:25 -0300

    não sei oq esta errado mas to tentando ;w; I'm about to cry, (joke)

--- challenges.js
+++ challenges.js
@@ -45,21 +45,39 @@ function highestCount(arrayNumber){
     }
     return result;
 }
-
 //Função 07
+catAndMouse(5 , 10, 6)
 function catAndMouse(mouse, cat1, cat2){
-    let gato1= (cat1 - mouse)
-    let gato2= (cat2 - mouse)
+    let gato1= (cat1 - mouse);
+    let gato2= (cat2 - mouse);
     if(gato1 > gato2){
         //console.log(cat2)
-        return cat1;
+        return cat2;
     }else if(gato2 > gato1){
         //console.log(cat1)
-        return cat2;
+        return cat1;
     }else if(gato1 === gato2){
         var mengs= "os gatos trombam e o rato foge"
         //console.log(mengs)
-        return mengs;
+        /return mengs;
     }
 }
 //Função 08
+function fizzBuzz( entrada ){
+    if(typeof entrada !== 'number'){
+        return 'Não é um numero';
+    }
+    if((entrada % 3 === 0) && (entrada % 5 === 0)){
+        let fb= "fizzbuzz";
+    }
+    if((entrada % 3 !== 0) && (entrada % 5 !== 0)){
+        let dif= "bug!"
+    }
+    if((entrada % 3 === 0)){
+        let string1= "fizz";
+    }else if((entrada % 5 === 0)){
+        let string2= "buzz";
+    }
+
+}
+
