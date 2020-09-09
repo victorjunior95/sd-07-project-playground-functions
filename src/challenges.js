e8548f1a9bb476754bfa2cdefcf9c8c921668919
mayconfelipes <mayconfelipe.dev@gmail.com>
2020-09-08 23:59:10 -0300

    Requisito 7. Go Trybe! :rocket:

--- src/challenges.js
+++ src/challenges.js
@@ -46,9 +46,14 @@ function highestCount(numeros) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  if ((mouse - cat1) < (mouse - cat2)){
+  cat1 = Math.abs((cat1 -= mouse));
+  cat2 = Math.abs((cat2 -= mouse));
+  if (cat1 < cat2){
   return cat1;
-  } else return cat2;
+  } else if (cat1 > cat2) {
+    return cat2;
+  }
+   else return "os gatos trombam e o rato foge";
 }
 
 // Desafio 8
