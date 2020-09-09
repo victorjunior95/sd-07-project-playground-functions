bb1769fe4325c052fe855af0874b8aabc3c97a36
Damiem <damiemblackwood@gmail.com>
2020-09-08 22:58:25 -0300

    primeira quase la -w-

--- challenges.js
+++ challenges.js
@@ -1,19 +1,63 @@
-//Voltar nesta função mais tarde :c
+compareTrue(2,2)
+//Voltar nesta função 1 mais tarde :c
 function compareTrue(parameters1 , parameters2){
-    if(parameters1 && parameters2 == true){
-        let resultado = true;
-        console.log(resultado)
+    if(parameters1 && parameters2 === true){
+        return true;
     }else {
-        let resultado = false;
-        console.log(resultado)
+        return false;
     }
 }
-
-//Função funcionando por enquanto :3
-function calcArea(base,Height){
-    let bases= base
+calcArea(1,5)
+//Função 2 funcionando por enquanto :3
+function calcArea(base, Height){
+    let b= base
     let altura= Height
-    let soma= base*altura/2
-    let resultado= soma
+    let soma= (base*altura)/2
     return soma;
+    //console.log(resultado)
 }
+
+//Função 03
+function splitSentence(stringName){
+    return stringName.split(" ");
+}
+
+
+//Função 04
+function concatName(arrayString){
+    let first = arrayString[0];
+    let last =  arrayString[arrayString.length-1];
+    let fullName= last + ", " + first;
+    return fullName;
+}
+
+//Função 05
+function footballPoints(wins, ties){
+    let sumWin = wins * 3;
+    let sumTies = ties * 1;
+    let sun = sumWins + sumTies;
+return sun
+}
+
+//Função 06
+function highestCount()
+
+//Função 07
+catAndMouse(5, 4, 4)
+function catAndMouse(mouse, cat1, cat2){
+    let gato1= (cat1 - mouse)
+    let gato2= (cat2 - mouse)
+    if(gato1 > gato2){
+        //console.log(cat2)
+        return cat1
+    }else if(gato2 > gato1){
+        //console.log(cat1)
+        return cat2
+    }else if(gato1 === gato2){
+        var mengs= "os gatos trombam e o rato foge"
+        //console.log(mengs)
+        return mengs;
+    }
+}
+//Função 08
+
