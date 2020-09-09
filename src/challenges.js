b9ed5f37aa4b001cd75246807afc77b4e6ff3b7e
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-09-08 22:19:28 -0300

    Fixing code climate

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,11 @@
 // Desafio 1
 function compareTrue(bol1, bol2) {
   if (bol1 === true && bol2 === true) {
-      return true;
+  return true;
   } else {
-      return false;
+  return false;
   }
-} 
+}
 // Desafio 2
 function calcArea(base, height) {
   let area = (base * height) / 2;
@@ -34,14 +34,14 @@ function footballPoints(wins, ties) {
 // Desafio 6
 function highestCount(array) {
   let arrayResult = [];
-  for (i = 0; i < array.length; i += 1) {
+  for (let i = 0; i < array.length; i += 1) {
     if (i === 0) {
-        arrayResult.push(array[0])
-    } else if (i != 0 && array[i] == arrayResult[0]) {
-        arrayResult.push(array[i]);
-    } else if (i != 0 && array[i] > arrayResult[0]) {
+      arrayResult.push(array[0])
+    } else if (i != 0 && array[i] === arrayResult[0]) {
+      arrayResult.push(array[i]);
+    } else if (i !== 0 && array[i] > arrayResult[0]) {
       arrayResult = [];
-      //arrayResult.push(array[i]);
+      arrayResult.push(array[i]);
     }
   }
   return arrayResult.length;
@@ -66,23 +66,23 @@ function fizzBuzz(array) {
   let arrayResult = [];
   for (let i = 0; i < array.length; i += 1) {
     if ((array[i] % 3 === 0) && (array[i] % 5 === 0)) {
-        arrayResult.push('fizzBuzz');
+      arrayResult.push('fizzBuzz');
     } else if (array[i] % 3 === 0) {
-        arrayResult.push('fizz');
+      arrayResult.push('fizz');
     } else if (array[i] % 5 === 0) {
-        arrayResult.push('buzz');
+      arrayResult.push('buzz');
     } else {
-        arrayResult.push('bug!');
+      arrayResult.push('bug!');
     }
   }
   return arrayResult;
 }
-// Desafio 9 
+// Desafio 9
 function encode(string) {
   let array = string.split('');
   for (let i = 0; i < array.length; i += 1) {
-    if (array[i] == 'a') {
-      array[i] = 1; 
+    if (array[i] === 'a') {
+      array[i] = 1;
     } else if (array[i] === 'e') {
       array[i] = 2;
     } else if (array[i] === 'i') {
@@ -99,7 +99,7 @@ function decode(string) {
   let array = string.split('');
   for (let i = 0; i < array.length; i += 1) {
     if (array[i] === 1) {
-      array[i] = 'a'; 
+      array[i] = 'a';
     } else if (array[i] === 2) {
       array[i] = 'e';
     } else if (array[i] === 3) {
@@ -118,80 +118,80 @@ function techList(array, name) {
   if (array[0] === undefined) {
     result = 'Vazio!';
   } else if (array !== []) {
-    //let result = [];
-      for (let i = 0; i < array.length; i += 1) {
-        let object = { tech: array[i], name: name };
-        result.push(object);
-        object = {};
-        }
+    let result = [];
+    for (let i = 0; i < array.length; i += 1) {
+      let object = { tech: array[i], name: name };
+      result.push(object);
+      object = {};
+    }
     result.sort((a, b) => (a.tech > b.tech) ? 1 : -1); // To sort the object by a property I searched in the following link: https://flaviocopes.com/how-to-sort-array-of-objects-by-property-javascript/
     return result;
   }
   return result;
 }
-// Desafio 11 
+// Desafio 11
 function generatePhoneNumber(array) {
   if (array.length !== 11) {
     return 'Array com tamanho incorreto.';
   } else {
     let arrayResult = [];
     for (let i = 0; i < array.length; i += 1) {
-        let value = array[i]
-        let cont0 = 0;
-        let cont1 = 0;
-        let cont2 = 0;
-        let cont3 = 0;
-        let cont4 = 0;
-        let cont5 = 0;
-        let cont6 = 0;
-        let cont7 = 0;
-        let cont8 = 0;
-        let cont9 = 0;
+      let value = array[i]
+      let cont0 = 0;
+      let cont1 = 0;
+      let cont2 = 0;
+      let cont3 = 0;
+      let cont4 = 0;
+      let cont5 = 0;
+      let cont6 = 0;
+      let cont7 = 0;
+      let cont8 = 0;
+      let cont9 = 0;
 
-        for (let j = 0; j <= array.length; j += 1) {
-              if (cont0 >= 3 || cont1 >= 3 || cont2 >= 3 || cont3 >= 3 || cont4 >= 3 || cont5 >= 3 || cont6 >= 3 || cont7 >= 3 || cont8 >= 3 || cont9 >= 3) {
-               return 'não é possível gerar um número de telefone com esses valores';
-             } else if (array[j] === 0) {
-               cont0 = cont0 += 1;
-             } else if (array[j] === 1) {
-               cont1 = cont1 += 1;
-             } else if (array[j] === 2) {
-               cont2 = cont2 += 1;
-             } else if (array[j] === 3) {
-               cont3 = cont3 += 1;
-             } else if (array[j] === 4) {
-               cont4 = cont4 += 1;
-             } else if (array[j] === 5) {
-               cont5 = cont5 += 1;
-             } else if (array[j] === 6) {
-               cont6 = cont6 += 1;
-             } else if (array[j] === 7) {
-               cont7 = cont7 += 1;
-             } else if (array[j] === 8) {
-               cont8 = cont8 += 1;
-             } else if (array[j] === 9) {
-               cont9 = cont9 += 1;
-             }
-            }    
-          if (value < 0 || value > 9) {
-            return 'não é possível gerar um número de telefone com esses valores';
-          } else if (i === 0) {
-            arrayResult.push('(');
-            arrayResult.push(array[i]);
-          } else if (i === 1) {
-            arrayResult.push(array[i]);
-            arrayResult.push(')');
-            arrayResult.push(' ');
-          } else if (i === 6) {
-            arrayResult.push(array[i]);
-            arrayResult.push('-')
-          } else if (i !== 0 && i !== 1 && i !== 6) {
-            arrayResult.push(array[i]);
-          }
+      for (let j = 0; j <= array.length; j += 1) {
+        if (cont0 >= 3 || cont1 >= 3 || cont2 >= 3 || cont3 >= 3 || cont4 >= 3 || cont5 >= 3 || cont6 >= 3 || cont7 >= 3 || cont8 >= 3 || cont9 >= 3) {
+        return 'não é possível gerar um número de telefone com esses valores';
+        } else if (array[j] === 0) {
+        cont0 = cont0 += 1;
+        } else if (array[j] === 1) {
+        cont1 = cont1 += 1;
+        } else if (array[j] === 2) {
+        cont2 = cont2 += 1;
+        } else if (array[j] === 3) {
+        cont3 = cont3 += 1;
+        } else if (array[j] === 4) {
+        cont4 = cont4 += 1;
+        } else if (array[j] === 5) {
+        cont5 = cont5 += 1;
+        } else if (array[j] === 6) {
+        cont6 = cont6 += 1;
+        } else if (array[j] === 7) {
+        cont7 = cont7 += 1;
+        } else if (array[j] === 8) {
+        cont8 = cont8 += 1;
+        } else if (array[j] === 9) {
+        cont9 = cont9 += 1;
         }
-      let result = arrayResult.join('');
-      return result;
+      }    
+        if (value < 0 || value > 9) {
+          return 'não é possível gerar um número de telefone com esses valores';
+        } else if (i === 0) {
+          arrayResult.push('(');
+          arrayResult.push(array[i]);
+        } else if (i === 1) {
+          arrayResult.push(array[i]);
+          arrayResult.push(')');
+          arrayResult.push(' ');
+        } else if (i === 6) {
+          arrayResult.push(array[i]);
+          arrayResult.push('-')
+        } else if (i !== 0 && i !== 1 && i !== 6) {
+          arrayResult.push(array[i]);
+        }
     }
+    let result = arrayResult.join('');
+    return result;
+  }
 }
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
