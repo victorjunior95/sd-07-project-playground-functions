41229fe41d723e4c0ffc2ed4b83004ea43c12808
vicoliveir <victorhugoqxd@gmail.com>
2020-09-09 14:00:38 -0300

    mudando true e false desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -2,10 +2,10 @@
     let num1 = 1;
     let num2 = 2;
 function compareTrue(num1,num2) {
-  if(num1==true && num2 == true){
+  if(num1>=0 && num2>=0){
     return true;
   }
-  else if(num1==true && num2==false){
+  else if(num1>=0 && num2<0){
     return false; 
   }
   
