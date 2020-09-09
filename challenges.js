047bfe35ca71167aab241636c01f7a33fbf05b6c
Damiem <damiemblackwood@gmail.com>
2020-09-09 13:36:40 -0300

    corrigido parte 2-w-

--- challenges.js
+++ challenges.js
@@ -84,4 +84,48 @@ function fizzBuzz( numbers ){
     }
     return saida;
 }
-
+// Desafio 9
+function encode() {
+    // seu código aqui
+  }
+  function decode() {
+    // seu código aqui
+  }
+  
+  // Desafio 10
+  function techList() {
+    // seu código aqui
+  }
+  
+  // Desafio 11
+  function generatePhoneNumber() {
+    // seu código aqui
+  }
+  
+  // Desafio 12
+  function triangleCheck() {
+    // seu código aqui
+  }
+  
+  // Desafio 13
+  function hydrate() {
+    // seu código aqui
+  }
+  
+  
+  module.exports = {
+    calcArea,
+    catAndMouse,
+    compareTrue,
+    concatName,
+    decode,
+    encode,
+    fizzBuzz,
+    footballPoints,
+    generatePhoneNumber,
+    techList,
+    highestCount,
+    hydrate,
+    splitSentence,
+    triangleCheck,
+  }
