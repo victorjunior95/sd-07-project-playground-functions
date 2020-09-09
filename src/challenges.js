a6c283def6a371aa5f96fa2006ad18b1ba0f2fac
vicoliveir <victorhugoqxd@gmail.com>
2020-09-09 13:24:06 -0300

    tentando mais uma vez desafio 1, editando condição true or false

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,8 @@
 // Desafio 1
     let num1 = 1;
     let num2 = 2;
-function compareTrue(num1,num2,num3) {
-    if(num1 < num2 && num2 > num1){
+function compareTrue(num1,num2) {
+    if(num1 > num2 && num2 > num1){
       return true;
     }
     else {
