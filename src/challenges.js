5628d5773eee81cfaba1da10bd3f3814f1bad111
Vanessa Oliveira <vanessanaara@gmail.com>
2020-09-10 13:40:36 -0300

    Retirando a linha 180

--- src/challenges.js
+++ src/challenges.js
@@ -177,8 +177,6 @@ function generatePhoneNumber(phoneArray) {
   return phoneNumbers;
 }
 
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
-
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   let cond1 = (lineA < lineB + lineC) && (lineB < lineC + lineA) && (lineC < lineB + lineA);
