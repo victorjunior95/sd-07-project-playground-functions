cc6e3855d2db0890bc157be86abf2848711e1cb1
victorvalim <victor.valim.c@gmail.com>
2020-09-10 10:49:11 -0300

    Ajustes desafio 6, 7, 8 part.3

--- src/challenges.js
+++ src/challenges.js
@@ -31,7 +31,7 @@ function splitSentence(string) {
 // Desafio 4
 function concatName(array) {
   // seu código aqui
-  let string = "array[array.length -1] , array[0]";
+  let string = array[array.length -1] + " ," + array[0] ;
   return string;
 }
 
