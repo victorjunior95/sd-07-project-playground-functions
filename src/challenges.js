0188c6b7b23c37f9a18f03c2c9d36f3f410da509
mayconfelipes <mayconfelipe.dev@gmail.com>
2020-09-10 13:38:16 -0300

    Requisito 12. GO TRYBE! :rocket:

--- src/challenges.js
+++ src/challenges.js
@@ -127,8 +127,14 @@ function generatePhoneNumber(arr) {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  lineA = Math.abs(lineA);
+  lineB = Math.abs(lineB);
+  lineC = Math.abs(lineC);
+  if (lineA > lineB + lineC || lineB > lineA + lineC || lineC > lineA + lineB) {
+    return false;
+  }
+  return true;
 }
 
 // Desafio 13
