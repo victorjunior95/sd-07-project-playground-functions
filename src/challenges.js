664de901ec15ab928778214b3fd2308f528efd32
spawnred <bruno.spawnhell@gmail.com>
2020-09-11 01:41:45 -0300

    item 11

--- src/challenges.js
+++ src/challenges.js
@@ -161,8 +161,37 @@ function techList(array,name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  if(array.length != 11){
+      return "Array com tamanho incorreto."
+  }
+  let array2 = [];
+
+  for(let i = 0; i < array.length; i++){
+    let cont = 0;  
+      let rep = false;
+
+      for(let z = 0; z < array2.length; z++){ // parte de código copiado de https://pt.stackoverflow.com/questions/230209/como-verificar-e-imprimir-valores-que-se-repetem-em-um-vetor
+          if(array2[z] == array[i]){
+          rep = true
+          }
+      }  
+      if(rep == false){
+          array2.push(array[i])
+          for(let j = 0; j < array.length; j++){
+              if(array[i] == array[j]){ 
+                  cont++
+                  if(cont>=3 || array[i]<0 || array[i]>9) {
+                      return "não é possível gerar um número de telefone com esses valores"
+              }
+              }
+    
+          }
+          
+    }
+
+  }
+  return `(${array[0]}${array[1]})${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`
 }
 
 // Desafio 12
