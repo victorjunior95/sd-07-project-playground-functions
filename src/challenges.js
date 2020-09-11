877f3f115c866054c0c6ebc723260a7c9903900e
spawnred <bruno.spawnhell@gmail.com>
2020-09-11 01:57:08 -0300

    item 11

--- src/challenges.js
+++ src/challenges.js
@@ -191,7 +191,7 @@ function generatePhoneNumber(array) {
     }
 
   }
-  return `(${array[0]}${array[1]})${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]} `
+  return `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`
 }
 
 // Desafio 12
