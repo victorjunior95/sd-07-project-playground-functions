2582cc8b06f1f887f09d25be338521619b48572c
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2020-09-13 09:44:16 -0300

    working in clodeclamite 9

--- src/challenges.js
+++ src/challenges.js
@@ -18,7 +18,7 @@ function splitSentence(string) {
 }
 // Desafio 4
 function concatName(array) {
-  let string = `${array[array.length - 1]}, ${ array[0] }`;
+  let string = `${array[array.length - 1]}, ${array[0]}`;
   return string;
 }
 // Desafio 5
